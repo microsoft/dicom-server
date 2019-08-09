6884c39ea0870f064d9c6e588666790cd667b466
REDMOND\itarapov <itarapov@microsoft.com>
2019-08-09 15:44:13 -0700

    Base classes boilerplate

---
+++ src/Microsoft.Health.Dicom.Core/Features/Resources/Retrieve/BitmapRendering/NetCoreImage.cs
@@ -0,0 +1,37 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Dicom.Imaging;
+using Dicom.Imaging.Render;
+using Dicom.IO;
+
+namespace Microsoft.Health.Dicom.Core.Features.Resources.Retrieve.BitmapRendering
+{
+    public sealed class NetCoreImage : ImageBase<Bitmap>
+    {
+        public NetCoreImage(int width, int height, PinnedIntArray pixels, Bitmap image)
+            : base(width, height, pixels, image)
+        {
+        }
+
+        public override void Render(int components, bool flipX, bool flipY, int rotation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DrawGraphics(IEnumerable<IGraphic> graphics)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IImage Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Microsoft.Health.Dicom.Core/Features/Resources/Retrieve/BitmapRendering/NetCoreImageManager.cs
@@ -0,0 +1,20 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using Dicom.Imaging;
+
+namespace Microsoft.Health.Dicom.Core.Features.Resources.Retrieve.BitmapRendering
+{
+    public sealed class NetCoreImageManager : ImageManager
+    {
+        public override bool IsDefault { get; }
+
+        protected override IImage CreateImageImpl(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
