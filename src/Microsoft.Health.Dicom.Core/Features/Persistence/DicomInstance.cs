3cdd389688b418c084961c377255449be12a93d8
Smitha Saligrama <smithasa@microsoft.com>
2020-03-18 17:08:00 -0700

    More delete of unused files after the switch to sql

--- src/Microsoft.Health.Dicom.Core/Features/Persistence/DicomInstance.cs
+++ src/Microsoft.Health.Dicom.Core/Features/Persistence/DicomInstance.cs
@@ -6,18 +6,25 @@
 using System;
 using Dicom;
 using EnsureThat;
+using Microsoft.Health.Dicom.Core.Features.Validation;
+using Newtonsoft.Json;
 
 namespace Microsoft.Health.Dicom.Core.Features.Persistence
 {
-    public class DicomInstance : IEquatable<DicomInstance>
+    public class DicomInstance
     {
         private const StringComparison EqualsStringComparison = StringComparison.Ordinal;
 
+        [JsonConstructor]
         public DicomInstance(string studyInstanceUID, string seriesInstanceUID, string sopInstanceUID)
         {
-            EnsureArg.IsNotNull(studyInstanceUID, nameof(studyInstanceUID));
-            EnsureArg.IsNotNull(seriesInstanceUID, nameof(seriesInstanceUID));
-            EnsureArg.IsNotNull(sopInstanceUID, nameof(sopInstanceUID));
+            // Run the instance identifiers through the regular expression check.
+            EnsureArg.Matches(studyInstanceUID, DicomIdentifierValidator.IdentifierRegex, nameof(studyInstanceUID));
+            EnsureArg.Matches(seriesInstanceUID, DicomIdentifierValidator.IdentifierRegex, nameof(seriesInstanceUID));
+            EnsureArg.Matches(sopInstanceUID, DicomIdentifierValidator.IdentifierRegex, nameof(sopInstanceUID));
+            EnsureArg.IsNotEqualTo(studyInstanceUID, seriesInstanceUID, nameof(seriesInstanceUID));
+            EnsureArg.IsNotEqualTo(sopInstanceUID, studyInstanceUID, nameof(sopInstanceUID));
+            EnsureArg.IsNotEqualTo(sopInstanceUID, seriesInstanceUID, nameof(sopInstanceUID));
 
             StudyInstanceUID = studyInstanceUID;
             SeriesInstanceUID = seriesInstanceUID;
@@ -30,29 +37,6 @@ namespace Microsoft.Health.Dicom.Core.Features.Persistence
 
         public string SopInstanceUID { get; }
 
-        public bool Equals(DicomInstance instance)
-        {
-            if (instance == null)
-            {
-                return false;
-            }
-
-            return StudyInstanceUID.Equals(instance.StudyInstanceUID, EqualsStringComparison) &&
-                        SeriesInstanceUID.Equals(instance.SeriesInstanceUID, EqualsStringComparison) &&
-                        SopInstanceUID.Equals(instance.SopInstanceUID, EqualsStringComparison);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as DicomInstance);
-        }
-
-        public override int GetHashCode()
-            => (StudyInstanceUID + SeriesInstanceUID + SopInstanceUID).GetHashCode(EqualsStringComparison);
-
-        public override string ToString()
-            => $"Study Instance UID: {StudyInstanceUID}, Series Instance UID: {SeriesInstanceUID}, SOP Instance UID {SopInstanceUID}";
-
         public static DicomInstance Create(DicomDataset dicomDataset)
         {
             EnsureArg.IsNotNull(dicomDataset, nameof(dicomDataset));
@@ -63,5 +47,23 @@ namespace Microsoft.Health.Dicom.Core.Features.Persistence
                 dicomDataset.GetSingleValueOrDefault(DicomTag.SeriesInstanceUID, string.Empty),
                 dicomDataset.GetSingleValueOrDefault(DicomTag.SOPInstanceUID, string.Empty));
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is DicomInstance identity)
+            {
+                return StudyInstanceUID.Equals(identity.StudyInstanceUID, EqualsStringComparison) &&
+                        SeriesInstanceUID.Equals(identity.SeriesInstanceUID, EqualsStringComparison) &&
+                        SopInstanceUID.Equals(identity.SopInstanceUID, EqualsStringComparison);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+            => (StudyInstanceUID + SeriesInstanceUID + SopInstanceUID).GetHashCode(EqualsStringComparison);
+
+        public override string ToString()
+            => $"Study Instance UID: {StudyInstanceUID}, Series Instance UID: {SeriesInstanceUID}, SOP Instance UID {SopInstanceUID}";
     }
 }
