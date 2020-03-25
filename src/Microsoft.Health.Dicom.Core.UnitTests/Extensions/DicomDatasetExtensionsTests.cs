2eb7e73251efa50227aff0f7827272a22cf5cf23
Jack Liu <jackliu@microsoft.com>
2020-03-25 13:41:05 -0700

    Addressed PR feedback.

--- src/Microsoft.Health.Dicom.Core.UnitTests/Extensions/DicomDatasetExtensionsTests.cs
+++ src/Microsoft.Health.Dicom.Core.UnitTests/Extensions/DicomDatasetExtensionsTests.cs
@@ -12,6 +12,8 @@ namespace Microsoft.Health.Dicom.Core.UnitTests.Extensions
 {
     public class DicomDatasetExtensionsTests
     {
+        private readonly DicomDataset _dataset = new DicomDataset();
+
         [Fact]
         public void GivenDicomTagDoesNotExist_WhenGetSingleOrDefaultIsCalled_ThenDefaultValueShouldBeReturned()
         {
@@ -27,41 +29,33 @@ namespace Microsoft.Health.Dicom.Core.UnitTests.Extensions
         {
             const string expectedValue = "IA";
 
-            var dataset = new DicomDataset();
+            _dataset.Add(DicomTag.InstanceAvailability, expectedValue);
 
-            dataset.Add(DicomTag.InstanceAvailability, expectedValue);
-
-            Assert.Equal(expectedValue, dataset.GetSingleValueOrDefault<string>(DicomTag.InstanceAvailability));
+            Assert.Equal(expectedValue, _dataset.GetSingleValueOrDefault<string>(DicomTag.InstanceAvailability));
         }
 
         [Fact]
         public void GivenNoDicomDateValue_WhenGetStringDateAsDateTimeIsCalled_ThenNullShouldBeReturned()
         {
-            var dataset = new DicomDataset();
-
-            Assert.Null(dataset.GetStringDateAsDateTime(DicomTag.StudyDate));
+            Assert.Null(_dataset.GetStringDateAsDateTime(DicomTag.StudyDate));
         }
 
         [Fact]
-        public void GivenAValidaDicomDateValue_WhenGetStringDateAsDateTimeIsCalled_ThenCorrectDateTimeShouldBeReturned()
+        public void GivenAValidDicomDateValue_WhenGetStringDateAsDateTimeIsCalled_ThenCorrectDateTimeShouldBeReturned()
         {
-            var dataset = new DicomDataset();
-
-            dataset.Add(DicomTag.StudyDate, "20200301");
+            _dataset.Add(DicomTag.StudyDate, "20200301");
 
             Assert.Equal(
                 new DateTime(2020, 3, 1, 0, 0, 0, 0, DateTimeKind.Utc),
-                dataset.GetStringDateAsDateTime(DicomTag.StudyDate));
+                _dataset.GetStringDateAsDateTime(DicomTag.StudyDate));
         }
 
         [Fact]
         public void GivenAnInvalidDicomDateValue_WhenGetStringDateAsDateTimeIsCalled_ThenNullShouldBeReturned()
         {
-            var dataset = new DicomDataset();
+            _dataset.Add(DicomTag.StudyDate, "2010");
 
-            dataset.Add(DicomTag.StudyDate, "2010");
-
-            Assert.Null(dataset.GetStringDateAsDateTime(DicomTag.StudyDate));
+            Assert.Null(_dataset.GetStringDateAsDateTime(DicomTag.StudyDate));
         }
     }
 }
