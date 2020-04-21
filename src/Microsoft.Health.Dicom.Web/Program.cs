8e557221ad912552238ad0106c290de888f62026
Smitha Saligrama <smithasa@microsoft.com>
2020-04-20 21:21:37 -0700

    Consuming the Health OSS nuget package

--- src/Microsoft.Health.Dicom.Web/Program.cs
+++ src/Microsoft.Health.Dicom.Web/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
-using Microsoft.Health.DevelopmentIdentityProvider;
+using Microsoft.Health.Development.IdentityProvider;
 
 namespace Microsoft.Health.Dicom.Web
 {
--- src/Microsoft.Health.Dicom.Web/Startup.cs
+++ src/Microsoft.Health.Dicom.Web/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Health.DevelopmentIdentityProvider;
+using Microsoft.Health.Development.IdentityProvider;
 
 namespace Microsoft.Health.Dicom.Web
 {
