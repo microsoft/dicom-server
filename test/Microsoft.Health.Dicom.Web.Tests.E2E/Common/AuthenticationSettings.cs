3fffe4a00185c498745204badaace4debe4e9b42
Smitha Saligrama <smithasa@microsoft.com>
2020-04-24 13:19:59 -0700

    Fix E2E tests

--- test/Microsoft.Health.Dicom.Web.Tests.E2E/Common/AuthenticationSettings.cs
+++ test/Microsoft.Health.Dicom.Web.Tests.E2E/Common/AuthenticationSettings.cs
@@ -7,14 +7,33 @@ using static Microsoft.Health.Dicom.Tests.Common.EnvironmentVariables;
 
 namespace Microsoft.Health.Dicom.Web.Tests.E2E.Common
 {
+    /// <summary>
+    /// Gets the Authentication settings to run E2E tests
+    /// In PR Pipelines: The environment variables are set
+    /// In Dev Env: It is defaulted to in-prc identity provider settings
+    /// </summary>
     public static class AuthenticationSettings
     {
-        public static string Scope => GetEnvironmentVariableWithDefault("Scope", Provider.Audience);
+        public static string Scope => GetEnvironmentVariableWithDefault("security_scope", Provider.Audience);
 
-        public static string Resource => GetEnvironmentVariableWithDefault("Resource", Provider.Audience);
+        public static string Resource => GetEnvironmentVariableWithDefault("security_resource", Provider.Audience);
 
-        public static string TokenUrl => GetEnvironmentVariableWithDefault("security_tokenUrl", string.Empty);
+        public static string TokenUrl => GetEnvironmentVariableWithDefault("security_tokenUrl", "https://localhost/connect/token");
 
-        public static bool SecurityEnabled => !string.IsNullOrWhiteSpace(TokenUrl);
+        /// <summary>
+        /// Set this env in identity provider #73715
+        /// </summary>
+        public static bool SecurityEnabled
+        {
+            get
+            {
+                if (bool.TryParse(GetEnvironmentVariableWithDefault("security_enabled", bool.FalseString), out bool result))
+                {
+                    return result;
+                }
+
+                return false;
+            }
+        }
     }
 }
