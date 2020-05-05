10c4ec8e1f09521bcfce5d7edf8784a536ef2f39
Smitha Saligrama <smithago@users.noreply.github.com>
2020-05-04 18:29:20 -0700

    ChangeFeed: Initial contract between store and service (#100)
    * Initial contract between store and service
    
    * Remove Dicom prefix. Add Replaced to state.
    
    Co-authored-by: Smitha Saligrama <smithasa@microsoft.com>

---
+++ src/Microsoft.Health.Dicom.Core/Features/ChangeFeed/ChangeFeedEntry.cs
@@ -0,0 +1,52 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using EnsureThat;
+
+namespace Microsoft.Health.Dicom.Core.Features.ChangeFeed
+{
+    /// <summary>
+    /// Represents each change feed entry of a change has retrieved from the store
+    /// </summary>
+    public class ChangeFeedEntry
+    {
+        public ChangeFeedEntry(
+            int sequence,
+            string studyInstanceUid,
+            string seriesInstanceUid,
+            string sopInstanceUid,
+            ChangeFeedAction action,
+            DateTime timeStamp,
+            ChangeFeedState state)
+        {
+            EnsureArg.IsNotNull(studyInstanceUid);
+            EnsureArg.IsNotNull(seriesInstanceUid);
+            EnsureArg.IsNotNull(sopInstanceUid);
+
+            Sequence = sequence;
+            StudyInstanceUid = studyInstanceUid;
+            SeriesInstanceUid = seriesInstanceUid;
+            SopInstanceUid = sopInstanceUid;
+            Action = action;
+            TimeStamp = timeStamp;
+            State = state;
+        }
+
+        public int Sequence { get; }
+
+        public string StudyInstanceUid { get; }
+
+        public string SeriesInstanceUid { get; }
+
+        public string SopInstanceUid { get; }
+
+        public ChangeFeedAction Action { get; }
+
+        public DateTime TimeStamp { get; }
+
+        public ChangeFeedState State { get; }
+    }
+}
---
+++ src/Microsoft.Health.Dicom.Core/Features/ChangeFeed/IChangeFeedStore.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Health.Dicom.Core.Features.ChangeFeed
+{
+    public interface IChangeFeedStore
+    {
+        Task<ChangeFeedEntry> GetChangeFeedLatestAsync(CancellationToken cancellationToken = default);
+
+        Task<IReadOnlyCollection<ChangeFeedEntry>> GetChangeFeedAsync(int offset, int limit, CancellationToken cancellationToken = default);
+    }
+}
