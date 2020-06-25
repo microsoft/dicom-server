d761501605bdf20bf3d5276a6e456c380a677789
brandonpollett <brpoll@microsoft.com>
2020-06-25 08:43:46 -0700

    Bring in missing schema changes. (#157)

--- src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Migrations/1.sql
+++ src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Migrations/1.sql
@@ -62,7 +62,6 @@ GO
 /*************************************************************
     Schema bootstrap
 **************************************************************/
-
 CREATE TABLE dbo.SchemaVersion
 (
     Version int PRIMARY KEY,
@@ -72,6 +71,27 @@ CREATE TABLE dbo.SchemaVersion
 INSERT INTO dbo.SchemaVersion
 VALUES
     (1, 'started')
+
+GO
+
+CREATE TABLE dbo.InstanceSchema
+(
+    Name varchar(64) COLLATE Latin1_General_100_CS_AS NOT NULL,
+    CurrentVersion int NOT NULL,
+    MaxVersion int NOT NULL,
+    MinVersion int NOT NULL,
+    Timeout datetime2(0) NOT NULL
+)
+
+CREATE UNIQUE CLUSTERED INDEX IXC_InstanceSchema ON dbo.InstanceSchema
+(
+    Name
+)
+
+CREATE NONCLUSTERED INDEX IX_InstanceSchema_Timeout ON dbo.InstanceSchema
+(
+    Timeout
+)
 
 GO
 
@@ -92,7 +112,7 @@ BEGIN
 
     SELECT MAX(Version)
     FROM SchemaVersion
-    WHERE Status = 'complete'
+    WHERE Status = 'completed'
 END
 GO
 
@@ -130,6 +150,147 @@ AS
         VALUES
             (@version, @status)
     END
+GO
+
+--
+-- STORED PROCEDURE
+--     Gets schema information given its instance name.
+--
+-- DESCRIPTION
+--     Retrieves the instance schema record from the InstanceSchema table that has the matching name.
+--
+-- PARAMETERS
+--     @name
+--         * The unique name for a particular instance
+--
+-- RETURN VALUE
+--     The matching record.
+--
+CREATE PROCEDURE dbo.GetInstanceSchemaByName
+    @name varchar(64)
+AS
+    SET NOCOUNT ON
+
+    SELECT CurrentVersion, MaxVersion, MinVersion, Timeout
+    FROM dbo.InstanceSchema
+    WHERE Name = @name
+GO
+
+--
+-- STORED PROCEDURE
+--     Update an instance schema.
+--
+-- DESCRIPTION
+--     Modifies an existing record in the InstanceSchema table.
+--
+-- PARAMETERS
+--    @name
+--         * The unique name for a particular instance
+--     @maxVersion
+--         * The maximum supported schema version for the given instance
+--     @minVersion
+--         * The minimum supported schema version for the given instance
+--     @addMinutesOnTimeout
+--         * The minutes to add
+--
+CREATE PROCEDURE dbo.UpsertInstanceSchema
+    @name varchar(64),
+    @maxVersion int,
+    @minVersion int,
+    @addMinutesOnTimeout int
+
+AS
+    SET NOCOUNT ON
+
+    DECLARE @timeout datetime2(0) = DATEADD(minute, @addMinutesOnTimeout, SYSUTCDATETIME())
+    DECLARE @currentVersion int = (SELECT COALESCE(MAX(Version), 0)
+                                    FROM dbo.SchemaVersion
+                                    WHERE  Status = 'completed'
+                                    OR Status = 'complete'
+                                    AND Version <= @maxVersion)
+    IF EXISTS(SELECT *
+                FROM dbo.InstanceSchema
+                WHERE Name = @name)
+    BEGIN
+        UPDATE dbo.InstanceSchema
+        SET CurrentVersion = @currentVersion, MaxVersion = @maxVersion, Timeout = @timeout
+        WHERE Name = @name
+
+        SELECT @currentVersion
+    END
+    ELSE
+    BEGIN
+        INSERT INTO dbo.InstanceSchema
+            (Name, CurrentVersion, MaxVersion, MinVersion, Timeout)
+        VALUES
+            (@name, @currentVersion, @maxVersion, @minVersion, @timeout)
+
+        SELECT @currentVersion
+    END
+GO
+
+--
+-- STORED PROCEDURE
+--     Delete instance schema information.
+--
+-- DESCRIPTION
+--     Delete all the expired records in the InstanceSchema table.
+--
+CREATE PROCEDURE dbo.DeleteInstanceSchema
+
+AS
+    SET NOCOUNT ON
+
+    DELETE FROM dbo.InstanceSchema
+    WHERE Timeout < SYSUTCDATETIME()
+
+GO
+
+--
+--  STORED PROCEDURE
+--      SelectCompatibleSchemaVersions
+--
+--  DESCRIPTION
+--      Selects the compatible schema versions
+--
+--  RETURNS
+--      The maximum and minimum compatible versions
+--
+CREATE PROCEDURE dbo.SelectCompatibleSchemaVersions
+
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT MAX(MinVersion), MIN(MaxVersion)
+    FROM dbo.InstanceSchema
+    WHERE Timeout > SYSUTCDATETIME()
+END
+GO
+
+--
+--  STORED PROCEDURE
+--      SelectCurrentVersionsInformation
+--
+--  DESCRIPTION
+--      Selects the current schema versions information
+--
+--  RETURNS
+--      The current versions, status and server names using that version
+--
+CREATE PROCEDURE dbo.SelectCurrentVersionsInformation
+
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    SELECT SV.Version, SV.Status, STRING_AGG(SCH.NAME, ',')
+    FROM dbo.SchemaVersion AS SV
+    LEFT OUTER JOIN dbo.InstanceSchema AS SCH
+    ON SV.Version = SCH.CurrentVersion
+    GROUP BY Version, Status
+
+END
 GO
 
 /*************************************************************
@@ -827,7 +988,7 @@ AS
     FROM @deletedInstances
 
     INSERT INTO dbo.ChangeFeed
-    (Timestamp, Action, StudyInstanceUid, SeriesInstanceUid, SopInstanceUid, OriginalWatermark)
+    (TimeStamp, Action, StudyInstanceUid, SeriesInstanceUid, SopInstanceUid, OriginalWatermark)
     SELECT @deletedDate, 1, StudyInstanceUid, SeriesInstanceUid, SopInstanceUid, Watermark
     FROM @deletedInstances
     WHERE Status = @createdStatus
@@ -839,7 +1000,6 @@ AS
     ON cf.StudyInstanceUid = d.StudyInstanceUid
         AND cf.SeriesInstanceUid = d.SeriesInstanceUid
         AND cf.SopInstanceUid = d.SopInstanceUid
-        AND d.Status = @createdStatus
 
     -- If this is the last instance for a series, remove the series
     IF NOT EXISTS ( SELECT  *
