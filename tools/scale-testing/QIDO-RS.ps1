63560d6c2df02105c0646847979330e95bb06520
snehabandla <62899142+snehabandla@users.noreply.github.com>
2020-08-21 13:44:18 -0700

    Scale Testing Solution (#216)
    * Adding in applications
    
    * Update to use constants instead of magic strings. (#182)
    
    * Update to dicom web client from existing project. (#181)
    
    * Add arm template for deploying scale testing infrastructure (#185)
    
    * Add service bus topic subscription to ARM Template (#191)
    
    * Adding in tools for automation (#198)
    
    * Removing references to hardcoded urls/configuration names
    
    * Adding in more details to the ReadMe
    
    * Fixing powershell scripts
    
    * Adding ability to build solutions in tools (#241)
    
    * Adding ability to build solutions in tools.
    Co-authored-by: snehabandla <snehabandla@users.noreply.github.com>
    Co-authored-by: brandonpollett <brpoll@microsoft.com>

---
+++ tools/scale-testing/QIDO-RS.ps1
@@ -0,0 +1,27 @@
+﻿$CurrentDirectory = (pwd).path
+
+$CommonModule = -join($CurrentDirectory, '\', 'Common.psm1')
+Import-Module $CommonModule -Force
+
+$txt = '.txt'
+$topicName = 'qido'
+
+$ConcurrentThreads = Read-Host -Prompt 'Input threads to run simultaneously for upload'
+
+build($QueryGeneratorProject)
+for($i = 0; $i -lt $ConcurrentThreads; $i++)
+{
+	$fileName = -join($CurrentDirectory, '\', $i, $txt)
+	$outputFileName = -join($CurrentDirectory, '\', $i, 'queries', $txt)
+	Start-Process -FilePath $QueryGeneratorApp -ArgumentList "$fileName $outputFileName" -RedirectStandardError "log.txt"
+}
+
+$null = Read-Host 'Press any key to continue once the QueryGenerator processes are completed.'
+
+build($MessageUploaderProject)
+for($i = 0; $i -lt $ConcurrentThreads; $i++)
+{    
+	$fileName = -join($CurrentDirectory, '\', $i, $txt)
+    $TotalCount = Get-Content $fileName | Measure-Object –Line
+	Start-Process -FilePath $MessageUploaderApp -ArgumentList "$topicName $fileName 0 $TotalCount.Line" -RedirectStandardError "log.txt"
+}
