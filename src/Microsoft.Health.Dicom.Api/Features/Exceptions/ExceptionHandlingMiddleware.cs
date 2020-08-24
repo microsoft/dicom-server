7b935693f4dae55dcf919696e25270cd73023fd6
Ali Shah <v-shaaal@microsoft.com>
2020-08-24 12:10:44 -0500

    Mapped audit exceptions in ExceptionHandlingMiddleware.

--- src/Microsoft.Health.Dicom.Api/Features/Exceptions/ExceptionHandlingMiddleware.cs
+++ src/Microsoft.Health.Dicom.Api/Features/Exceptions/ExceptionHandlingMiddleware.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Health.Abstractions.Exceptions;
+using Microsoft.Health.Dicom.Api.Features.Audit;
 using Microsoft.Health.Dicom.Core.Exceptions;
 using NotSupportedException = Microsoft.Health.Dicom.Core.Exceptions.NotSupportedException;
 
@@ -66,6 +67,8 @@ namespace Microsoft.Health.Dicom.Api.Features.Exceptions
             {
                 case ValidationException _:
                 case NotSupportedException _:
+                case AuditHeaderCountExceededException _:
+                case AuditHeaderTooLargeException _:
                     statusCode = HttpStatusCode.BadRequest;
                     break;
                 case ResourceNotFoundException _:
