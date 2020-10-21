1e2822c79e597ab731e520418e746deecf105412
Ali <67655573+v-shaaal@users.noreply.github.com>
2020-10-21 16:47:59 -0500

    Auditing Executed Messages in Middleware (#386)
    * Implemented AuditEgressLogger. All the LogExecuted messages are logged through AuditEgressLogger. Executed messages for DICOM are no longer written in two different places.
    
    * Update AuditHelper to match the expected method signature. Deleted AuditEgressLogger. Updated unit tests.
    
    * Updated references to healthcare shared components latest.
    
    * Updated references to point to latest healthcare shared components.
    
    * Addressed comments. Made sure all the tests are passing locally.

---
+++ src/Microsoft.Health.Dicom.Api.UnitTests/Features/Audit/AuditMiddlewareTests.cs
@@ -0,0 +1,61 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Health.Api.Features.Audit;
+using Microsoft.Health.Core.Features.Security;
+using NSubstitute;
+using Xunit;
+
+namespace Microsoft.Health.Dicom.Api.UnitTests.Features.Audit
+{
+    public class AuditMiddlewareTests
+    {
+        private readonly IClaimsExtractor _claimsExtractor = Substitute.For<IClaimsExtractor>();
+        private readonly IAuditHelper _auditHelper = Substitute.For<IAuditHelper>();
+
+        private readonly AuditMiddleware _auditMiddleware;
+
+        private readonly HttpContext _httpContext = new DefaultHttpContext();
+
+        public AuditMiddlewareTests()
+        {
+            _auditMiddleware = new AuditMiddleware(
+                httpContext => Task.CompletedTask,
+                _claimsExtractor,
+                _auditHelper);
+        }
+
+        [Fact]
+        public async Task GivenSuccess_WhenInvoked_ThenAuditLogShouldBeLogged()
+        {
+            _httpContext.Response.StatusCode = (int)HttpStatusCode.OK;
+
+            await _auditMiddleware.Invoke(_httpContext);
+
+            _auditHelper.Received(1).LogExecuted(_httpContext, _claimsExtractor, shouldCheckForAuthXFailure: true);
+        }
+
+        [Theory]
+        [InlineData(HttpStatusCode.NotAcceptable)]
+        [InlineData(HttpStatusCode.BadRequest)]
+        [InlineData(HttpStatusCode.Conflict)]
+        [InlineData(HttpStatusCode.InternalServerError)]
+        [InlineData(HttpStatusCode.NotFound)]
+        [InlineData(HttpStatusCode.ServiceUnavailable)]
+        [InlineData(HttpStatusCode.Unauthorized)]
+        [InlineData(HttpStatusCode.UnsupportedMediaType)]
+        public async Task GivenFailure_WhenInvoked_ThenAuditLogShouldBeLogged(HttpStatusCode statusCode)
+        {
+            _httpContext.Response.StatusCode = (int)statusCode;
+
+            await _auditMiddleware.Invoke(_httpContext);
+
+            _auditHelper.Received(1).LogExecuted(_httpContext, _claimsExtractor, shouldCheckForAuthXFailure: true);
+        }
+    }
+}
