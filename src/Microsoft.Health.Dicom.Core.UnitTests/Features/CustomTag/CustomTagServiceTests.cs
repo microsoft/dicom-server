a510c07096a157bd13138640a84165935d4b7a75
Peng Chen <67345894+pengchen0692@users.noreply.github.com>
2021-01-27 10:44:38 -0800

    Remove reindex from AddCustomTagService (#518)
    Remove reindex job from AddCustomTagService. Reindex will be implemented later as a feature.

--- src/Microsoft.Health.Dicom.Core.UnitTests/Features/CustomTag/CustomTagServiceTests.cs
+++ src/Microsoft.Health.Dicom.Core.UnitTests/Features/CustomTag/CustomTagServiceTests.cs
@@ -3,110 +3,53 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using Dicom;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Health.Dicom.Core.Exceptions;
 using Microsoft.Health.Dicom.Core.Features.CustomTag;
-using Microsoft.Health.Dicom.Core.Messages.CustomTag;
 using Microsoft.Health.Dicom.Core.UnitTests.Features.CustomTag;
 using NSubstitute;
-using NSubstitute.ExceptionExtensions;
 using Xunit;
 
 namespace Microsoft.Health.Dicom.Core.UnitTests.Features.ChangeFeed
 {
     public class CustomTagServiceTests
     {
-        private IReindexJob _reindexJob;
         private ICustomTagEntryValidator _customTagEntryValidator;
         private ICustomTagStore _customTagStore;
         private ICustomTagService _customTagService;
 
         public CustomTagServiceTests()
         {
-            _reindexJob = Substitute.For<IReindexJob>();
             _customTagEntryValidator = Substitute.For<ICustomTagEntryValidator>();
             _customTagStore = Substitute.For<ICustomTagStore>();
-            _customTagService = new CustomTagService(_customTagStore, _reindexJob, _customTagEntryValidator, NullLogger<CustomTagService>.Instance);
+            _customTagService = new CustomTagService(_customTagStore, _customTagEntryValidator, NullLogger<CustomTagService>.Instance);
         }
 
         [Fact]
         public async Task GivenValidInput_WhenAddCustomTagIsInvoked_ThenShouldSucceed()
         {
-            _customTagStore.GetLatestInstanceAsync(default).ReturnsForAnyArgs(1);
-            IEnumerable<CustomTagEntry> entries = new CustomTagEntry[]
-            {
-                DicomTag.ManufacturerModelName.BuildCustomTagEntry(),
-            };
-            AddCustomTagResponse response = await _customTagService.AddCustomTagAsync(entries);
+            DicomTag tag = DicomTag.DeviceSerialNumber;
+            CustomTagEntry entry = tag.BuildCustomTagEntry();
+            await _customTagService.AddCustomTagAsync(new CustomTagEntry[] { entry }, default);
 
-            _customTagEntryValidator.ReceivedWithAnyArgs()
-               .ValidateCustomTags(default);
-
-            await _customTagStore.ReceivedWithAnyArgs()
-                .GetLatestInstanceAsync(default);
-
-            await _reindexJob.ReceivedWithAnyArgs()
-                .ReindexAsync(default, default, default);
+            _customTagEntryValidator.ReceivedWithAnyArgs().ValidateCustomTags(default);
+            await _customTagStore.ReceivedWithAnyArgs().AddCustomTagsAsync(default, default);
         }
 
         [Fact]
-        public async Task GivenInvalidInput_WhenAddCustomTagIsInvoked_ThenShouldFailAtValidation()
+        public async Task GivenInvalidInput_WhenAddCustomTagIsInvoked_ThenStopAfterValidation()
         {
-            _customTagEntryValidator.WhenForAnyArgs(x => x.ValidateCustomTags(default))
-                .Throw(new Exception());
-            IEnumerable<CustomTagEntry> entries = new CustomTagEntry[]
-            {
-                DicomTag.ManufacturerModelName.BuildCustomTagEntry(),
-            };
+            DicomTag tag = DicomTag.DeviceSerialNumber;
+            CustomTagEntryValidationException exception = new CustomTagEntryValidationException(string.Empty);
+            CustomTagEntry entry = tag.BuildCustomTagEntry();
+            _customTagEntryValidator.WhenForAnyArgs(validator => validator.ValidateCustomTags(default))
+                .Throw(exception);
 
-            await Assert.ThrowsAsync<Exception>(() => _customTagService.AddCustomTagAsync(entries));
-
-            await _customTagStore.DidNotReceiveWithAnyArgs()
-                .AddCustomTagAsync(default, default, default, default, default);
-
-            await _reindexJob.DidNotReceiveWithAnyArgs()
-                .ReindexAsync(default, default, default);
-        }
-
-        [Fact]
-        public async Task GivenMultipleCustomTags_WhenFailInTheMiddle_ThenShouldFailAndRollback()
-        {
-            CustomTagEntry customTagEntry1 = DicomTag.ManufacturerModelName.BuildCustomTagEntry();
-            CustomTagEntry customTagEntry2 = DicomTag.PatientBirthDate.BuildCustomTagEntry();
-            IEnumerable<CustomTagEntry> entries = new CustomTagEntry[]
-            {
-                customTagEntry1,
-                customTagEntry2,
-            };
-
-            _customTagStore.AddCustomTagAsync(customTagEntry2.Path, customTagEntry2.VR, customTagEntry2.Level, CustomTagStatus.Reindexing, Arg.Any<CancellationToken>())
-                .Throws(new Exception());
-
-            await Assert.ThrowsAsync<Exception>(() => _customTagService.AddCustomTagAsync(entries));
-
-            await _customTagStore.ReceivedWithAnyArgs()
-                .DeleteCustomTagAsync(default, default);
-        }
-
-        [Fact]
-        public async Task GivenValidInput_WhenThereIsNoInstance_ThenShouldNotReinde()
-        {
-            _customTagStore.GetLatestInstanceAsync(default)
-                .ReturnsForAnyArgs((long?)null);
-
-            IEnumerable<CustomTagEntry> entries = new CustomTagEntry[]
-            {
-                DicomTag.ManufacturerModelName.BuildCustomTagEntry(),
-            };
-
-            AddCustomTagResponse response = await _customTagService.AddCustomTagAsync(entries);
-
-            await _reindexJob.DidNotReceiveWithAnyArgs()
-                .ReindexAsync(default, default, default);
+            await Assert.ThrowsAsync<CustomTagEntryValidationException>(() => _customTagService.AddCustomTagAsync(new CustomTagEntry[] { entry }, default));
+            _customTagEntryValidator.ReceivedWithAnyArgs().ValidateCustomTags(default);
+            await _customTagStore.DidNotReceiveWithAnyArgs().AddCustomTagsAsync(default, default);
         }
     }
 }
--- src/Microsoft.Health.Dicom.Core/Features/CustomTag/ICustomTagStore.cs
+++ src/Microsoft.Health.Dicom.Core/Features/CustomTag/ICustomTagStore.cs
@@ -3,46 +3,24 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Health.Dicom.Core.Features.CustomTag
 {
+    /// <summary>
+    /// The store saving custom tags.
+    /// </summary>
     public interface ICustomTagStore
     {
         /// <summary>
-        /// Add Custom tag into CustomTagStore.
+        /// Add custom tags into CustomTagStore
+        /// Notes: once moved to job framework, the return will jobid, to save development effort, just return task for now.
         /// </summary>
-        /// <param name="path">The tag path.</param>
-        /// <param name="vr">The tag VR.</param>
-        /// <param name="level">The tag level.</param>
-        /// <param name="status">The tag status.</param>
+        /// <param name="customTagEntries">The custom tag entries.</param>
         /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>The tag key.</returns>
-        Task<long> AddCustomTagAsync(string path, string vr, CustomTagLevel level, CustomTagStatus status, CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Update custom tag status.
-        /// </summary>
-        /// <param name="key">The tag key.</param>
-        /// <param name="status">The tag status.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>Result.</returns>
-        Task UpdateCustomTagStatusAsync(long key, CustomTagStatus status, CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Delete custom tag from CustomTagStore.
-        /// </summary>
-        /// <param name="key">The tag key.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>Result.</returns>
-        Task DeleteCustomTagAsync(long key, CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Get latest instance.
-        /// </summary>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>The watermark of latest instance.</returns>
-        Task<long?> GetLatestInstanceAsync(CancellationToken cancellationToken = default);
+        /// <returns>The task.</returns>
+        Task AddCustomTagsAsync(IEnumerable<CustomTagEntry> customTagEntries, CancellationToken cancellationToken = default);
     }
 }
