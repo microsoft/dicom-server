3ef3d5ff47df328869f92443df332177fb62774d
Peng Chen <penche@microsoft.com>
2021-02-17 11:38:40 -0800

    manually merge 2.sql

--- src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Migrations/2.sql
+++ src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Migrations/2.sql
@@ -527,6 +527,17 @@ KEY INDEX IXC_CustomTagPersonName_WatermarkAndTagKey
 WITH STOPLIST = OFF;
 
 /*************************************************************
+    The user defined type for AddCustomTagsInput
+*************************************************************/
+CREATE TYPE dbo.AddCustomTagsInputTableType_1 AS TABLE
+(
+    TagPath                    VARCHAR(64),  -- Custom Tag Path. Each custom tag take 8 bytes, support upto 8 levels, no delimeter between each level.
+    TagVR                      VARCHAR(2),  -- Custom Tag VR.
+    TagLevel                   TINYINT  -- Custom Tag level. 0 -- Instance Level, 1 -- Series Level, 2 -- Study Level
+)
+GO
+
+/*************************************************************
     Sequence for generating sequential unique ids
 **************************************************************/
 
@@ -1092,66 +1103,7 @@ BEGIN
     ORDER BY Sequence DESC
 END
 GO
-/*************************************************************
-    SQL VERSION 2
-*************************************************************/
-/*************************************************************
-    TABLES
-*************************************************************/
 
-/*************************************************************
-    Custom Tag Table
-    Stores added custom tags
-    TagPath is represented without any delimiters and each level takes 8 bytes
-    TagLevel can be 0, 1 or 2 to represent Instance, Series or Study level
-**************************************************************/
-CREATE TABLE dbo.CustomTag (
-    TagKey                  BIGINT               NOT NULL, --PK
-    TagPath                 VARCHAR(64)          NOT NULL,
-    TagVR                   VARCHAR(2)           NOT NULL,
-    TagLevel                TINYINT              NOT NULL,
-    TagStatus               TINYINT              NOT NULL,
-)
-
-CREATE UNIQUE CLUSTERED INDEX IXC_CustomTag ON dbo.CustomTag
-(
-    TagKey
-)
-
-CREATE UNIQUE NONCLUSTERED INDEX IX_CustomTag_TagPath ON dbo.CustomTag
-(
-    TagPath
-)
-
-/*************************************************************
-    SEQUENCES
-*************************************************************/
-CREATE SEQUENCE dbo.TagKeySequence
-    AS BIGINT
-    START WITH 1
-    INCREMENT BY 1
-    MINVALUE 1
-    NO CYCLE
-    CACHE 10000
-GO
-
-/*************************************************************
-    USER DEFINED TYPES
-*************************************************************/
-/*************************************************************
-    The user defined type for AddCustomTagsInput
-*************************************************************/
-CREATE TYPE dbo.AddCustomTagsInputTableType_1 AS TABLE
-(
-    TagPath                    VARCHAR(64),  -- Custom Tag Path. Each custom tag take 8 bytes, support upto 8 levels, no delimeter between each level.
-    TagVR                      VARCHAR(2),  -- Custom Tag VR.
-    TagLevel                   TINYINT  -- Custom Tag level. 0 -- Instance Level, 1 -- Series Level, 2 -- Study Level
-)
-GO
-
-/*************************************************************
-    PROCEDURES
-*************************************************************/
 /***************************************************************************************/
 -- STORED PROCEDURE
 --     AddCustomTags
