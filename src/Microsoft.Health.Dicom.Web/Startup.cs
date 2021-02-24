6e12ff6f2ce94dc71f7444279cc40d046e7eb82c
Peng Chen <67345894+pengchen0692@users.noreply.github.com>
2021-02-24 10:17:27 -0800

    Fix bug which block service to start (#585)
    Fix bug which block service to start

--- src/Microsoft.Health.Dicom.Web/Startup.cs
+++ src/Microsoft.Health.Dicom.Web/Startup.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.Health.Development.IdentityProvider;
 
 namespace Microsoft.Health.Dicom.Web
 {
