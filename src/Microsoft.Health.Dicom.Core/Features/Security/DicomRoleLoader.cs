dd299f00a37401ea675a1e163d0cf4bc78cb1861
brandonpollett <brpoll@microsoft.com>
2021-03-09 14:54:42 -0800

    Code cleanup.

--- src/Microsoft.Health.Dicom.Core/Features/Security/DicomRoleLoader.cs
+++ src/Microsoft.Health.Dicom.Core/Features/Security/DicomRoleLoader.cs
@@ -4,6 +4,7 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using EnsureThat;
@@ -15,36 +16,38 @@ namespace Microsoft.Health.Dicom.Core.Features.Security
 {
     public class DicomRoleLoader : RoleLoader<DataActions>
     {
+        private Dictionary<string, DataActions> _dataActionsMap = new Dictionary<string, DataActions>();
+
         public DicomRoleLoader(AuthorizationConfiguration<DataActions> authorizationConfiguration, IHostEnvironment hostEnvironment)
             : base(authorizationConfiguration, hostEnvironment)
         {
+            // Loop through all the enums and pre-load them into a dictionary for mapping between string representation and enum
+            var enumType = typeof(DataActions);
+            foreach (var name in Enum.GetNames(enumType))
+            {
+                var enumMemberAttribute = ((EnumMemberAttribute[])enumType.GetField(name).GetCustomAttributes(typeof(EnumMemberAttribute), true)).Single();
+                _dataActionsMap.TryAdd(enumMemberAttribute.Value, Enum.Parse<DataActions>(name));
+            }
         }
 
         protected override Role<DataActions> RoleContractToRole(RoleContract roleContract)
         {
             EnsureArg.IsNotNull(roleContract, nameof(roleContract));
 
-            DataActions dataActions = roleContract.DataActions.Aggregate(default(DataActions), (acc, a) => acc | ToEnum<DataActions>(a));
-            DataActions notDataActions = roleContract.NotDataActions.Aggregate(default(DataActions), (acc, a) => acc | ToEnum<DataActions>(a));
+            DataActions dataActions = roleContract.DataActions.Aggregate(default(DataActions), (acc, a) => acc | ToEnum(a));
+            DataActions notDataActions = roleContract.NotDataActions.Aggregate(default(DataActions), (acc, a) => acc | ToEnum(a));
 
             return new Role<DataActions>(roleContract.Name, dataActions & ~notDataActions, roleContract.Scopes.Single());
         }
 
-        // TODO: See if there's a better way to do this than the StackOverflow answer
-        public static T ToEnum<T>(string str)
+        private DataActions ToEnum(string str)
         {
-            var enumType = typeof(T);
-            foreach (var name in Enum.GetNames(enumType))
+            if (_dataActionsMap.TryGetValue(str, out DataActions foundDataAction))
             {
-                var enumMemberAttribute = ((EnumMemberAttribute[])enumType.GetField(name).GetCustomAttributes(typeof(EnumMemberAttribute), true)).Single();
-                if (enumMemberAttribute.Value == str)
-                {
-                    return (T)Enum.Parse(enumType, name);
-                }
+                return foundDataAction;
             }
 
-            // throw exception or whatever handling you want or
-            return default(T);
+            throw new ArgumentOutOfRangeException($"Invalid data action supplied: {str}");
         }
     }
 }
