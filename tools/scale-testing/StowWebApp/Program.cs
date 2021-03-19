9777e8214c8358a42b26fb68a016f03a7cd3b75e
snehabandla <snehabandla@users.noreply.github.com>
2021-03-19 10:16:23 -0700

    Adding a web app to store instances at scale

--- tools/scale-testing/StowWebApp/Program.cs
+++ tools/scale-testing/StowWebApp/Program.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Threading.Tasks;
 using Common;
 using Dicom;
 using Microsoft.Health.Dicom.Client;
@@ -16,7 +17,7 @@ namespace StowWebApp
         private static Random rand;
         private static IDicomWebClient client;
 
-        public static async Task Main()
+        public static void Main()
         {
             using var httpClient = new HttpClient
             {
@@ -25,45 +26,57 @@ namespace StowWebApp
 
             SetupDicomWebClient(httpClient);
 
-            // update to total count/instances of web app.
+            // update to total count/instances to be uploaded.
             int totalCount = 1000;
+            int threadCount = 10;
 
+            Task[] tasks = new Task[10];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                tasks[i] = Task.Run(() => StoreManyInstances(totalCount / threadCount));
+            }
+
+            Task.WaitAll(tasks);
+        }
+
+        private static async Task StoreManyInstances(int count)
+        {
             int tracker = 0;
 
-            while (tracker < totalCount)
+            while (tracker < count)
             {
-                    List<(string, string, string)> instances = InstanceGenerator();
+                List<(string, string, string)> instances = InstanceGenerator();
 
-                    foreach ((string studyUID, string seriesUID, string instanceUID) inst in instances)
+                foreach ((string studyUID, string seriesUID, string instanceUID) inst in instances)
+                {
+                    // 400, 400, 100 - 16MB
+                    // 100, 100, 100 - 1MB
+                    // 100, 100, 50 - 0.5MB
+                    DicomFile dicomFile = Samples.CreateRandomDicomFileWithPixelData(
+                                inst.studyUID,
+                                inst.seriesUID,
+                                inst.instanceUID,
+                                rows: 100,
+                                columns: 100,
+                                frames: 50);
+                    try
                     {
-                        // 400, 400, 100 - 16MB
-                        // 100, 100, 100 - 1MB
-                        // 100, 100, 50 - 0.5MB
-                        DicomFile dicomFile = Samples.CreateRandomDicomFileWithPixelData(
-                                    inst.studyUID,
-                                    inst.seriesUID,
-                                    inst.instanceUID,
-                                    rows: 100,
-                                    columns: 100,
-                                    frames: 50);
-                        try
+                        DicomWebResponse<DicomDataset> response = await client.StoreAsync(new List<DicomFile>() { dicomFile });
+
+                        int statusCode = (int)response.StatusCode;
+                        if (statusCode != 409 && statusCode < 200 && statusCode > 299)
                         {
-
-                            DicomWebResponse<DicomDataset> response = await client.StoreAsync(new List<DicomFile>() { dicomFile }, cancellationToken: token);
-
-                            int statusCode = (int)response.StatusCode;
-                            if (statusCode != 409 && statusCode < 200 && statusCode > 299)
-                            {
-                                throw new HttpRequestException("Stow operation failed", null, response.StatusCode);
-                            }
+                            throw new HttpRequestException("Stow operation failed", null, response.StatusCode);
                         }
-                        catch (Exception exception)
-                        {
-                            Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
-                        }
-
-                        tracker++;
                     }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+                    }
+
+                    tracker++;
+                }
             }
         }
 
@@ -76,11 +89,15 @@ namespace StowWebApp
         {
             List<(string studyUID, string seriesUID, string instanceUID)> ret = new List<(string, string, string)>();
             string studyUid = DicomUID.Generate().UID;
+#pragma warning disable CA5394 // Do not use insecure randomness
             int series = rand.Next(1, 5);
+#pragma warning restore CA5394 // Do not use insecure randomness
             for (int i = 0; i < series; i++)
             {
                 string seriesUid = DicomUID.Generate().UID;
+#pragma warning disable CA5394 // Do not use insecure randomness
                 int instances = rand.Next(1, 7);
+#pragma warning restore CA5394 // Do not use insecure randomness
                 for (int j = 0; j < instances; j++)
                 {
                     string instanceUid = DicomUID.Generate().UID;
