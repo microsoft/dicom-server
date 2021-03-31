8c12cd7ffd0a0b82f99630b4857d2966dcc24cdc
Peng Chen <penche@microsoft.com>
2021-03-31 07:51:34 -0700

    trigger a new PR

--- src/Microsoft.Health.Dicom.Core/Exceptions/ExtendedQueryTagFeatureDisabledException.cs
+++ src/Microsoft.Health.Dicom.Core/Exceptions/ExtendedQueryTagFeatureDisabledException.cs
@@ -8,7 +8,7 @@ using System.Globalization;
 namespace Microsoft.Health.Dicom.Core.Exceptions
 {
     /// <summary>
-    /// Exception thrown when extended query tag feature is disabled.
+    /// Exception that is thrown when extended query tag feature is disabled.
     /// </summary>
     public class ExtendedQueryTagFeatureDisabledException : BadRequestException
     {
