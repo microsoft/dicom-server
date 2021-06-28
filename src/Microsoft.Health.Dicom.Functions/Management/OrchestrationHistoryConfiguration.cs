a6dfc151265689aa7c4f93b3c1e7853ddf471aef
Yue Dai <yue.dai@mail.mcgill.ca>
2021-06-28 13:42:57 -0400

    Durable Functions History Purge (#819)
    * Azure function that purges the history of durable functions that respects a set of conditions with Unit tests.

---
+++ src/Microsoft.Health.Dicom.Functions/Management/OrchestrationHistoryConfiguration.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace Microsoft.Health.Dicom.Functions.Management
+{
+    public class OrchestrationHistoryConfiguration
+    {
+        public const string SectionName = "OrchestrationHistory";
+
+        [Required]
+        [MinLength(1)]
+        public IReadOnlyCollection<OrchestrationRuntimeStatus> RuntimeStatuses { get; set; }
+
+        [Range(0, 1000)]
+        public int MinimumAgeDays { get; set; } = 30;
+
+        [Required]
+        public string PurgeFrequency { get; set; }
+
+        public const string PurgeFrequencyVariable =
+            "%" + AzureFunctionsJobHost.SectionName + ":" + SectionName + ":" + nameof(PurgeFrequency) + "%";
+    }
+}
---
+++ src/Microsoft.Health.Dicom.Functions/Management/PurgeOrchestrationInstanceHistory.cs
@@ -0,0 +1,88 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using EnsureThat;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.Health.Dicom.Functions.Management
+{
+    public class PurgeOrchestrationInstanceHistory
+    {
+        private readonly OrchestrationHistoryConfiguration _purgeConfig;
+        private readonly Func<DateTime> _getUtcNow;
+
+        public PurgeOrchestrationInstanceHistory(IOptions<OrchestrationHistoryConfiguration> cleanupOptions)
+            : this(cleanupOptions, () => DateTime.UtcNow)
+        { }
+
+        internal PurgeOrchestrationInstanceHistory(IOptions<OrchestrationHistoryConfiguration> cleanupOptions, Func<DateTime> getDateTimeUtcNow)
+        {
+            EnsureArg.IsNotNull(cleanupOptions?.Value, nameof(cleanupOptions));
+            EnsureArg.IsNotNull(getDateTimeUtcNow, nameof(getDateTimeUtcNow));
+            _purgeConfig = cleanupOptions.Value;
+            _getUtcNow = getDateTimeUtcNow;
+        }
+
+        [FunctionName(nameof(PurgeOrchestrationInstanceHistory))]
+        public async Task Run(
+            [TimerTrigger(OrchestrationHistoryConfiguration.PurgeFrequencyVariable)] TimerInfo myTimer,
+            [DurableClient] IDurableOrchestrationClient client,
+            ILogger log,
+            CancellationToken hostCancellationToken)
+        {
+            EnsureArg.IsNotNull(client, nameof(client));
+            EnsureArg.IsNotNull(myTimer, nameof(myTimer));
+            EnsureArg.IsNotNull(log, nameof(log));
+
+            List<string> orchestrationInstanceIdList = new List<string>();
+
+            log.LogInformation("Purging orchestration instance history at: {Timestamp}", _getUtcNow());
+            if (myTimer.IsPastDue)
+            {
+                log.LogWarning("Current function invocation is later than scheduled.");
+            }
+
+            // Specify conditions for orchestration instances.
+            OrchestrationStatusQueryCondition condition = new OrchestrationStatusQueryCondition
+            {
+                RuntimeStatus = _purgeConfig.RuntimeStatuses,
+                CreatedTimeFrom = DateTime.MinValue,
+                CreatedTimeTo = _getUtcNow().AddDays(-_purgeConfig.MinimumAgeDays),
+                ContinuationToken = null
+            };
+
+            do
+            {
+                OrchestrationStatusQueryResult listOfOrchestrators =
+                   await client.ListInstancesAsync(condition, hostCancellationToken);
+                condition.ContinuationToken = listOfOrchestrators.ContinuationToken;
+
+                // Loop through the orchestration instances and purge them.
+                foreach (DurableOrchestrationStatus orchestration in listOfOrchestrators.DurableOrchestrationState)
+                {
+                    orchestrationInstanceIdList.Add(orchestration.InstanceId);
+                    await client.PurgeInstanceHistoryAsync(orchestration.InstanceId);
+                }
+            } while (condition.ContinuationToken != null);
+
+            if (orchestrationInstanceIdList.Count != 0)
+            {
+                log.LogInformation("{Count} Durable Functions cleaned up successfully.", orchestrationInstanceIdList.Count);
+                log.LogDebug("List of cleaned instance IDs: {list}", String.Join(", ", orchestrationInstanceIdList));
+            }
+            else
+            {
+                log.LogInformation("No Orchestration instances found within given conditions.");
+            }
+        }
+    }
+}
