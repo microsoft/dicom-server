b7908daa543ff8f624bca14b3165fc0bb7689f59
Peng Chen <penche@microsoft.com>
2021-07-08 09:38:51 -0700

    Update 4.diff.sql

--- src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Migrations/4.diff.sql
+++ src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Migrations/4.diff.sql
@@ -1,3 +1,45 @@
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION
+GO
+
+/*************************************************************
+    Stored procedures for adding an instance.
+**************************************************************/
+--
+-- STORED PROCEDURE
+--     GetInstancesByWatermarkRange
+--
+-- DESCRIPTION
+--     Get instances by given watermark range.
+--
+-- PARAMETERS
+--     @startWatermark
+--         * The inclusive start watermark.
+--     @endWatermark
+--         * The inclusive end watermark.
+--     @status
+--         * The instance status.
+-- RETURN VALUE
+--     The instance identifiers.
+------------------------------------------------------------------------
+CREATE OR ALTER PROCEDURE dbo.GetInstancesByWatermarkRange(
+    @startWatermark BIGINT,
+    @endWatermark BIGINT,
+    @status TINYINT
+)
+AS
+    SET NOCOUNT ON
+    SET XACT_ABORT ON
+    SELECT StudyInstanceUid,
+           SeriesInstanceUid,
+           SopInstanceUid,
+           Watermark
+    FROM dbo.Instance
+    WHERE Watermark BETWEEN @startWatermark AND @endWatermark
+          AND Status = @status
+GO
+
 /***************************************************************************************/
 -- STORED PROCEDURE
 --     AddExtendedQueryTags
@@ -40,39 +82,6 @@ AS
 
     COMMIT TRANSACTION
 GO
-/*************************************************************
-    Stored procedures for adding an instance.
-**************************************************************/
---
--- STORED PROCEDURE
---     GetInstancesByWatermarkRange
---
--- DESCRIPTION
---     Get instances by given watermark range.
---
--- PARAMETERS
---     @startWatermark
---         * The inclusive start watermark.
---     @endWatermark
---         * The inclusive end watermark.
---     @status
---         * The instance status.
--- RETURN VALUE
---     The instance identifiers.
-------------------------------------------------------------------------
-CREATE OR ALTER PROCEDURE dbo.GetInstancesByWatermarkRange(
-    @startWatermark BIGINT,
-    @endWatermark BIGINT,
-    @status TINYINT
-)
-AS
-    SET NOCOUNT ON
-    SET XACT_ABORT ON
-    SELECT StudyInstanceUid,
-           SeriesInstanceUid,
-           SopInstanceUid,
-           Watermark
-    FROM dbo.Instance
-    WHERE Watermark BETWEEN @startWatermark AND @endWatermark
-          AND Status = @status
+
+COMMIT TRANSACTION
 GO
