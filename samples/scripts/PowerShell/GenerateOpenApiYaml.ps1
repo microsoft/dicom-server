3f51a0093a5c1bb7b8ec897ee3ccecd96b9b322f
Abdullah Islam <31964511+abdullah248@users.noreply.github.com>
2021-08-02 17:04:14 -0500

    pr comments and move to own stage

--- samples/scripts/PowerShell/GenerateOpenApiYaml.ps1
+++ samples/scripts/PowerShell/GenerateOpenApiYaml.ps1
@@ -3,31 +3,32 @@
 Generates the OpenApi doc for the specified version and compares it with the baseline to make sure no breaking changes are introduced
 .Parameter WorkingDir
 The working directory
-.PARAMETER OutputPathofDll
+.PARAMETER AssemblyDir
 Path for the web projects dll
-.PARAMETER OutputPathOfOpenApiDoc
-Path to output the OpenApi Doc (in yaml)
 .PARAMETER Versions
 Api versions to generate the OpenApiDoc for and compare with baseline
+.PARAMETER SwashbuckleCLIVersion
+Version of SwashbuckleCLI to use
 #>
 
 param(
     [string]$WorkingDir,
 
-    [string]$OutputPathofDll,
+    [string]$AssemblyDir,
 
-    [string]$OutputPathOfOpenApiDoc,
+    [String[]]$Versions,
 
-    [String[]] $Versions
+    [string]$SwashbuckleCLIVersion = '6.1.4'
 )
-foreach($Version in $Versions){
+foreach ($Version in $Versions)
+{
     write-host "Generating Yaml file for $Version"
 
     dotnet new tool-manifest --force
-    dotnet tool install --version 6.1.4 Swashbuckle.AspNetCore.Cli
-    dotnet swagger tofile --yaml --output $WorkingDir/$OutputPathOfOpenApiDoc/$Version.yaml $WorkingDir/$OutputPathofDll $Version
+    dotnet tool install --version $SwashbuckleCLIVersion Swashbuckle.AspNetCore.Cli
+    dotnet swagger tofile --yaml --output "$WorkingDir/src/Microsoft.Health.Dicom.Web/bin/$Version.yaml" "$WorkingDir/$AssemblyDir" $Version
 
     write-host "Running comparison with baseline for version $Version"
-    docker run --rm -t -v ${WorkingDir}:/dicom-server openapitools/openapi-diff:latest /dicom-server/swagger/$Version/swagger.yaml /dicom-server/$OutputPathOfOpenApiDoc/$version.yaml --fail-on-incompatible
+    docker run --rm -t -v ${WorkingDir}:/dicom-server openapitools/openapi-diff:latest "/dicom-server/swagger/$Version/swagger.yaml" "/dicom-server/src/Microsoft.Health.Dicom.Web/bin/$version.yaml" --fail-on-incompatible
 
 }
