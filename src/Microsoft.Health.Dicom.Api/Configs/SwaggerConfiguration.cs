77bcd51a82177227fd9db5e250a46843277d3fb0
brandonpollett <brpoll@microsoft.com>
2021-08-13 10:18:59 -0700

    Updates to swagger.

---
+++ src/Microsoft.Health.Dicom.Api/Configs/SwaggerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using Microsoft.OpenApi.Models;
+
+namespace Microsoft.Health.Dicom.Api.Configs
+{
+    public class SwaggerConfiguration
+    {
+        public Uri ServerUri { get; set; }
+
+        public string Title { get; } = "Medical Imaging Server for DICOM";
+
+        public OpenApiLicense License { get; } = new OpenApiLicense();
+    }
+}
