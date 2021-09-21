fee0e48a7a65e978597e59a4e8fb021a98766c82
Will Sugarman <will.sugarman@microsoft.com>
2021-09-21 13:30:46 -0700

    Update IDicomWebClient for Extended Query Tags + Errors (#1041)
    - Update IDicomWebClient with new paginated APIs for getting extended query tags and their errors
    - Update data models to incorporate references to operation and errors
    - Fix errors return type and DI
    - Update E2E test

---
+++ src/Microsoft.Health.Dicom.Client/Models/ExtendedQueryTagError.cs
@@ -0,0 +1,40 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+
+namespace Microsoft.Health.Dicom.Client.Models
+{
+    /// <summary>
+    /// Represents a problem that arose when re-indexing the given DICOM instance.
+    /// </summary>
+    public class ExtendedQueryTagError
+    {
+        /// <summary>
+        /// Gets or sets the DICOM study instance UID.
+        /// </summary>
+        public string StudyInstanceUid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DICOM series instance UID.
+        /// </summary>
+        public string SeriesInstanceUid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the DICOM SOP instance UID.
+        /// </summary>
+        public string SopInstanceUid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date and time when the error was found.
+        /// </summary>
+        public DateTime CreatedTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the localized error message.
+        /// </summary>
+        public string ErrorMessage { get; set; }
+    }
+}
