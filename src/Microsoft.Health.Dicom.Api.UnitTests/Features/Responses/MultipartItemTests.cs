bdc064067e40737902f3ba935373cac3e0f2a899
Josiah Vinson <jovinson@microsoft.com>
2021-10-13 23:40:00 -0700

    Adding unit test

---
+++ src/Microsoft.Health.Dicom.Api.UnitTests/Features/Responses/MultipartItemTests.cs
@@ -0,0 +1,33 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.IO;
+using Microsoft.Health.Dicom.Api.Features.Responses;
+using Xunit;
+
+namespace Microsoft.Health.Dicom.Api.UnitTests.Features.Responses
+{
+    public class MultipartItemTests
+    {
+        [Theory]
+        [InlineData("application/dicom")]
+        [InlineData("application/dicom+json")]
+        public void GivenAValidContentType_ThenMultipartItemShouldIncludeHeader(string contentType)
+        {
+            var multipartItem = new MultipartItem(contentType, Stream.Null);
+
+            Assert.Equal(multipartItem.Content.Headers.ContentType.ToString(), contentType);
+        }
+
+        [Theory]
+        [InlineData("application/dicom;foo=bar")]
+        [InlineData("@#$%")]
+        public void GivenAnInvalidContentType_ThenMultipartItemShouldThrow(string contentType)
+        {
+            Assert.Throws<FormatException>(() => new MultipartItem(contentType, Stream.Null));
+        }
+    }
+}
