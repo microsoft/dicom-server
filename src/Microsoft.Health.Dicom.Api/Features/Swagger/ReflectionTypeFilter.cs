7d26723997915088c8ee1d63fa1cd9e1d50b9c01
Will Sugarman <will.sugarman@microsoft.com>
2021-11-30 15:37:51 -0800

    Added more comments

--- src/Microsoft.Health.Dicom.Api/Features/Swagger/ReflectionTypeFilter.cs
+++ src/Microsoft.Health.Dicom.Api/Features/Swagger/ReflectionTypeFilter.cs
@@ -12,8 +12,12 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Microsoft.Health.Dicom.Api.Features.Swagger
 {
+    // The ReflectionTypeFilter is used to remove types added mistakenly by the Swashbuckle library.
+    // Swashbuckle mistakenly assumes the DICOM server will serialize Type properties present on the fo-dicom types,
+    // so this filter removes erroneously added properties in addition to their recursively added data models.
     internal class ReflectionTypeFilter : IDocumentFilter
     {
+        // Get all built-in generic collections (and IEnumerable<T>)
         private static readonly HashSet<Type> CollectionTypes = typeof(List<>).Assembly
             .ExportedTypes
             .Where(x => x.Namespace == "System.Collections.Generic")
@@ -48,11 +52,13 @@ namespace Microsoft.Health.Dicom.Api.Features.Swagger
             var exposedTypes = new HashSet<Type>();
             UpdateExposedTypes(t, exposedTypes);
 
+            // The OpenApiSchema type only has the type names
             return exposedTypes.Select(x => x.Name).ToHashSet(StringComparer.Ordinal);
         }
 
         private static void UpdateExposedTypes(Type t, HashSet<Type> exposed)
         {
+            // Get all public instance properties present in the Type that may be discovered via reflection by Swashbuckle
             foreach (Type propertyType in t.GetProperties(BindingFlags.Public | BindingFlags.Instance).Select(x => GetExposedType(x.PropertyType)))
             {
                 if (!IsBuiltInType(propertyType) && exposed.Add(propertyType))
@@ -72,6 +78,8 @@ namespace Microsoft.Health.Dicom.Api.Features.Swagger
 
         private static Type GetExposedType(Type t)
         {
+            // If we're serializing a collection to JSON, it will be represented as a JSON array.
+            // So the type we're really concerned with is the element type contained in the collection.
             if (t.IsArray)
             {
                 t = t.GetElementType();
