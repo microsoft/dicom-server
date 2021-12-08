ee176401117ef0d0d9e8232b037a36763d674c25
brandonpollett <brpoll@microsoft.com>
2021-12-08 14:47:28 -0500

    Minor updates.

--- converter/dicom-cast/src/Microsoft.Health.DicomCast.TableStorage/Configs/TableDataStoreConfiguration.cs
+++ converter/dicom-cast/src/Microsoft.Health.DicomCast.TableStorage/Configs/TableDataStoreConfiguration.cs
@@ -9,10 +9,19 @@ namespace Microsoft.Health.DicomCast.TableStorage.Configs
 {
     public class TableDataStoreConfiguration
     {
+        /// <summary>
+        /// The storage table connection string to use. Setting this assumes the use of an account key.
+        /// </summary>
         public string ConnectionString { get; set; }
 
+        /// <summary>
+        /// The endpoint of the table storage account. Setting this assumes the use of a managed identity to communicate.
+        /// </summary>
         public Uri EndpointUri { get; set; }
 
+        /// <summary>
+        /// Optional parameter to use to specify the clientId of the managed identity to use.
+        /// </summary>
         public string ManagedIdentityClientId { get; set; }
     }
 }
