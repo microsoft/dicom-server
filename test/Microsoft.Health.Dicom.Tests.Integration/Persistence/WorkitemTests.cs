38177ca76fb14ae98494d85824b8afa33a3229ae
Karthik Balasubramanian 👍 <kabalas@microsoft.com>
2022-01-05 16:26:25 -0800

    Updated sql generator

--- test/Microsoft.Health.Dicom.Tests.Integration/Persistence/WorkitemTests.cs
+++ test/Microsoft.Health.Dicom.Tests.Integration/Persistence/WorkitemTests.cs
@@ -3,10 +3,11 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System.Linq;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Dicom;
+using Microsoft.Health.Dicom.Core.Extensions;
 using Microsoft.Health.Dicom.Core.Features.ExtendedQueryTag;
 using Microsoft.Health.Dicom.Core.Features.Partition;
 using Microsoft.Health.Dicom.Core.Features.Workitem;
@@ -27,11 +28,18 @@ namespace Microsoft.Health.Dicom.Tests.Integration.Persistence
         public async Task WhenValidWorkitemIsCreated_CreationSucceeds()
         {
             string workitemUid = "2.25.1234";
+            DicomTag tag2 = DicomTag.PatientName;
 
             var dataset = new WorkitemDataset();
             dataset.Add(DicomTag.SOPInstanceUID, workitemUid);
+            dataset.Add(DicomTag.PatientName, "Foo");
 
-            long workitemKey = await _fixture.WorkitemStore.AddWorkitemAsync(DefaultPartition.Key, dataset, Enumerable.Empty<QueryTag>(), CancellationToken.None);
+            var queryTags = new List<QueryTag>()
+            {
+                new QueryTag(new ExtendedQueryTagStoreEntry(2, tag2.GetPath(), tag2.GetDefaultVR().Code, null, QueryTagLevel.Instance, ExtendedQueryTagStatus.Ready, QueryStatus.Enabled,0)),
+            };
+
+            long workitemKey = await _fixture.WorkitemStore.AddWorkitemAsync(DefaultPartition.Key, dataset, queryTags, CancellationToken.None);
 
             Assert.True(workitemKey > 0, "WorkitemKey not returned.");
         }
