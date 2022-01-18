0d30d17f2f68dae7a73f0ab76e61a803395241b1
Will Sugarman <will.sugarman@microsoft.com>
2022-01-18 13:20:32 -0500

    Fix serialization and enable E2E tests

---
+++ src/Microsoft.Health.Dicom.Operations.Client/Serialization/MessageSerializerSettingsFactory.cs
@@ -0,0 +1,25 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Newtonsoft.Json;
+
+namespace Microsoft.Health.Dicom.Operations.Client.Serialization
+{
+    // TODO: Migrate to common package
+    internal class MessageSerializerSettingsFactory : IMessageSerializerSettingsFactory
+    {
+        public JsonSerializerSettings CreateJsonSerializerSettings()
+        {
+            // Based on the framework settings:
+            // https://github.com/Azure/azure-functions-durable-extension/blob/v2.6.0/src/WebJobs.Extensions.DurableTask/MessageSerializerSettingsFactory.cs
+            return new JsonSerializerSettings
+            {
+                TypeNameHandling = TypeNameHandling.None,
+                DateParseHandling = DateParseHandling.None,
+            };
+        }
+    }
+}
