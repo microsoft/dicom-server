3933012c437740924420364539cbccd50525021c
Karthik Balasubramanian 👍 <kabalas@microsoft.com>
2022-01-26 20:16:46 -0800

    Fix tests

--- src/Microsoft.Health.Dicom.Tests.Common/Samples.cs
+++ src/Microsoft.Health.Dicom.Tests.Common/Samples.cs
@@ -219,7 +219,7 @@ namespace Microsoft.Health.Dicom.Tests.Common
             }
 
             ds.Add(DicomTag.AffectedSOPInstanceUID, workitemUid ?? TestUidGenerator.Generate());
-            ds.Add(DicomTag.ScheduledProcedureStepPriority, Guid.NewGuid().ToString("N"));
+            ds.Add(DicomTag.ScheduledProcedureStepPriority, Guid.NewGuid().ToString("N").Substring(0, 14));
             ds.Add(DicomTag.ProcedureStepLabel, Guid.NewGuid().ToString("N"));
             ds.Add(DicomTag.WorklistLabel, Guid.NewGuid().ToString("N"));
             ds.Add(DicomTag.ScheduledStationNameCodeSequence, new DicomDataset());
@@ -230,17 +230,17 @@ namespace Microsoft.Health.Dicom.Tests.Common
             ds.Add(DicomTag.ScheduledProcedureStepStartDateTime, DateTime.Now);
             ds.Add(DicomTag.ExpectedCompletionDateTime, DateTime.Now);
             ds.Add(DicomTag.ScheduledWorkitemCodeSequence, new DicomDataset());
-            ds.Add(DicomTag.InputReadinessState, Guid.NewGuid().ToString("N").Substring(0, 15));
+            ds.Add(DicomTag.InputReadinessState, Guid.NewGuid().ToString("N").Substring(0, 14));
             ds.Add(DicomTag.PatientName, Guid.NewGuid().ToString("N"));
             ds.Add(DicomTag.PatientID, TestUidGenerator.Generate());
             ds.Add(DicomTag.PatientBirthDate, DateTime.Now.ToShortDateString());
-            ds.Add(DicomTag.PatientSex, Guid.NewGuid().ToString("N").Substring(0, 15));
+            ds.Add(DicomTag.PatientSex, Guid.NewGuid().ToString("N").Substring(0, 14));
             ds.Add(DicomTag.AdmissionID, Guid.NewGuid().ToString("N"));
             ds.Add(DicomTag.IssuerOfAdmissionIDSequence, new DicomDataset());
             ds.Add(DicomTag.ReferencedRequestSequence, new DicomDataset());
-            ds.Add(DicomTag.AccessionNumber, Guid.NewGuid().ToString("N").Substring(0, 15));
+            ds.Add(DicomTag.AccessionNumber, Guid.NewGuid().ToString("N").Substring(0, 14));
             ds.Add(DicomTag.IssuerOfAccessionNumberSequence, new DicomDataset());
-            ds.Add(DicomTag.RequestedProcedureID, Guid.NewGuid().ToString("N").Substring(0, 15));
+            ds.Add(DicomTag.RequestedProcedureID, Guid.NewGuid().ToString("N").Substring(0, 14));
             ds.Add(DicomTag.RequestingService, Guid.NewGuid().ToString("N"));
             ds.Add(DicomTag.ReplacedProcedureStepSequence, new DicomDataset());
             ds.Add(DicomTag.ProcedureStepState, string.Empty);
