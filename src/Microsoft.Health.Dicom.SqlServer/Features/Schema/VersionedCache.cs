279f85537d53a97e1f68df7fe1ff4a459af3fbca
Will Sugarman <will.sugarman@microsoft.com>
2022-02-16 12:20:45 -0800

    Allow Store Implementations whose Versions are Less than the Minimum (#1362)
    - Fix the VersionedCache<T> to allow implementations before min version

--- src/Microsoft.Health.Dicom.SqlServer/Features/Schema/VersionedCache.cs
+++ src/Microsoft.Health.Dicom.SqlServer/Features/Schema/VersionedCache.cs
@@ -38,18 +38,19 @@ namespace Microsoft.Health.Dicom.SqlServer.Features.Schema
 
         private async Task<T> ResolveAsync(CancellationToken cancellationToken = default)
         {
-            SchemaVersion version = await _schemaVersionResolver.GetCurrentVersionAsync(cancellationToken);
+            SchemaVersion current = await _schemaVersionResolver.GetCurrentVersionAsync(cancellationToken);
 
             // Return the latest version of the entity. If the entity is not found, throw an exception.
-            T value = _entities.Where(entity => entity.Version <= version && (int)entity.Version >= SchemaVersionConstants.Min).FirstOrDefault();
+            T value = _entities.FirstOrDefault(x => x.Version <= current);
 
-            if (value != null) return value;
-
-            string msg = version == SchemaVersion.Unknown
+            if (value == null)
+            {
+                throw new InvalidSchemaVersionException(current == SchemaVersion.Unknown
                     ? DicomSqlServerResource.UnknownSchemaVersion
-                    : string.Format(CultureInfo.InvariantCulture, DicomSqlServerResource.SchemaVersionOutOfRange, version);
+                    : string.Format(CultureInfo.InvariantCulture, DicomSqlServerResource.SchemaVersionOutOfRange, current));
+            }
 
-            throw new InvalidSchemaVersionException(msg);
+            return value;
         }
     }
 }
