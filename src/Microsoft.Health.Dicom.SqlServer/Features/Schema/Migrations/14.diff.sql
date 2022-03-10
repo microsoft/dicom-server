46c7102be8d7f129c770bcd9f664fbfaa9f3c73f
Peng Chen <penche@microsoft.com>
2022-03-10 10:19:14 -0800

    Update 14.diff.sql

--- src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Migrations/14.diff.sql
+++ src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Migrations/14.diff.sql
@@ -1,3 +1,4 @@
+
 /****************************************************************************************
 Guidelines to create migration scripts - https://github.com/microsoft/healthcare-shared-components/tree/master/src/Microsoft.Health.SqlServer/SqlSchemaScriptsGuidelines.md
 
