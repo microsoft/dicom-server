87f1498879abf8e9ba06e512e70c06cfa3ff0659
brandonpollett <brpoll@microsoft.com>
2022-03-22 08:41:26 -0400

    Basic idea

---
+++ test/SqlSchemaRunner/LibrarySchemaClient.cs
@@ -0,0 +1,83 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.ObjectModel;
+using EnsureThat;
+using Microsoft.Health.SqlServer.Features.Schema;
+using Microsoft.Health.SqlServer.Features.Schema.Manager;
+using Microsoft.Health.SqlServer.Features.Schema.Manager.Model;
+using Microsoft.Health.SqlServer.Features.Schema.Model;
+
+namespace SqlSchemaRunner;
+
+public class LibrarySchemaClient : ISchemaClient
+{
+    private readonly IScriptProvider _scriptProvider;
+    private readonly ISchemaDataStore _schemaDataStore;
+    private readonly SchemaInformation _schemaInformation;
+
+    public LibrarySchemaClient(IScriptProvider scriptProvider, ISchemaDataStore schemaDataStore, SchemaInformation schemaInformation)
+    {
+        _scriptProvider = EnsureArg.IsNotNull(scriptProvider);
+        _schemaDataStore = EnsureArg.IsNotNull(schemaDataStore);
+        _schemaInformation = EnsureArg.IsNotNull(schemaInformation);
+    }
+
+    public void SetUri(Uri uri)
+    {
+    }
+
+    public async Task<List<CurrentVersion>> GetCurrentVersionInformationAsync(CancellationToken cancellationToken)
+    {
+        List<CurrentVersionInformation>? currentVersions = await _schemaDataStore.GetCurrentVersionAsync(cancellationToken);
+
+        IEnumerable<CurrentVersion> versions = currentVersions.Select(version => new CurrentVersion(version.Id, version.Status.ToString(), new ReadOnlyCollection<string>(version.Servers)));
+
+        return versions.ToList();
+    }
+
+    public Task<string> GetScriptAsync(Uri scriptUri, CancellationToken cancellationToken)
+    {
+        EnsureArg.IsNotNull(scriptUri);
+        int segment = int.Parse(scriptUri.Segments.Last().Replace(".sql", string.Empty, StringComparison.OrdinalIgnoreCase));
+
+        return Task.FromResult(_scriptProvider.GetMigrationScript(segment, true));
+    }
+
+    public Task<string> GetDiffScriptAsync(Uri diffScriptUri, CancellationToken cancellationToken)
+    {
+        EnsureArg.IsNotNull(diffScriptUri);
+        int segment = int.Parse(diffScriptUri.Segments.Last().Replace(".sql", string.Empty, StringComparison.OrdinalIgnoreCase));
+
+        return Task.FromResult(_scriptProvider.GetMigrationScript(segment, false));
+    }
+
+    public async Task<CompatibleVersion> GetCompatibilityAsync(CancellationToken cancellationToken)
+    {
+        CompatibleVersions? compatibleVersions = await _schemaDataStore.GetLatestCompatibleVersionsAsync(cancellationToken);
+        return new CompatibleVersion(compatibleVersions.Min, compatibleVersions.Max);
+    }
+
+    public Task<List<AvailableVersion>> GetAvailabilityAsync(CancellationToken cancellationToken)
+    {
+        var availableVersions = new List<AvailableVersion>();
+
+        var currentVersion = _schemaInformation.Current ?? 1;
+        for (var version = currentVersion; version <= _schemaInformation.MaximumSupportedVersion; version++)
+        {
+            var routeValues = new Dictionary<string, object> { { "id", version } };
+            string scriptUri = $"{version}.sql";
+            string diffScriptUri = string.Empty;
+            if (version > 1)
+            {
+                diffScriptUri = $"{version}.diff.sql";
+            }
+
+            availableVersions.Add(new AvailableVersion(version, scriptUri, diffScriptUri));
+        }
+
+        return Task.FromResult(availableVersions);
+    }
+}
---
+++ test/SqlSchemaRunner/Program.cs
@@ -0,0 +1,89 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Health.Dicom.SqlServer.Features.Schema;
+using Microsoft.Health.Extensions.DependencyInjection;
+using Microsoft.Health.SqlServer;
+using Microsoft.Health.SqlServer.Configs;
+using Microsoft.Health.SqlServer.Features.Schema;
+using Microsoft.Health.SqlServer.Features.Schema.Manager;
+using Microsoft.Health.SqlServer.Registration;
+using SchemaManager.Core;
+
+namespace SqlSchemaRunner;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        var configuration = builder.Build();
+
+        // Add SqlServer services
+        services.AddOptions();
+        services.AddHttpClient();
+
+        services.AddSqlServerConnection(c => configuration.GetSection(SqlServerDataStoreConfiguration.SectionName).Bind(c));
+        services.AddSqlServerManagement<SchemaVersion>();
+        // TODO: this won't work in OSS if the AuthenticationType is set to ManagedIdentity
+        services.AddSingleton<ISqlConnectionBuilder, DefaultSqlConnectionBuilder>();
+        services.AddSingleton<ISqlConnectionStringProvider, DefaultSqlConnectionStringProvider>();
+        services.AddSingleton<IBaseSchemaRunner, BaseSchemaRunner>();
+        services.AddSingleton<ISchemaManagerDataStore, SchemaManagerDataStore>();
+        services.AddSingleton<ISchemaClient, LibrarySchemaClient>();
+        services.AddSingleton<ISchemaManager, SqlSchemaManager>();
+        services.AddSingleton(SqlConfigurableRetryFactory.CreateNoneRetryProvider());
+
+        services.AddSingleton<ISchemaClient, LibrarySchemaClient>();
+        services.AddLogging(configure => configure.AddConsole());
+
+        services.Add(provider => new SchemaInformation(SchemaVersionConstants.Min, SchemaVersionConstants.Max))
+            .Singleton()
+            .AsSelf();
+
+        var serviceProvider = services.BuildServiceProvider();
+
+        var schemaManager = serviceProvider.GetService<ISchemaManager>();
+        var logger = serviceProvider.GetService<ILogger>();
+        var connectionStringProvider = serviceProvider.GetService<ISqlConnectionStringProvider>();
+        var schemaManagerDataStore = serviceProvider.GetService<ISchemaManagerDataStore>();
+        var schemaInformation = serviceProvider.GetService<SchemaInformation>();
+
+        if (schemaManagerDataStore != null)
+        {
+            int version = schemaManagerDataStore.GetCurrentSchemaVersionAsync(default).Result;
+            if (version != 0)
+            {
+                if (schemaInformation != null)
+                {
+                    schemaInformation.Current = version;
+                }
+            }
+        }
+
+        var server = new Uri("https://localhost");
+        var output = schemaManager?.GetAvailableSchema(server);
+
+        var connectionstring = connectionStringProvider?.GetSqlConnectionString(default).Result;
+
+        var currentVersions = schemaManager?.GetCurrentSchema(connectionstring, server, default).Result;
+
+        if (currentVersions != null)
+        {
+            var currentVersion = currentVersions.FirstOrDefault();
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
