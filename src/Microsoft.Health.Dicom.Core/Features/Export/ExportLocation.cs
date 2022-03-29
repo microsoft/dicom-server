24d7b562db20a4c5bfc874b36ac66d92aa0ad17e
Will Sugarman <will.sugarman@microsoft.com>
2022-03-29 11:59:34 -0700

    Models WIP

---
+++ src/Microsoft.Health.Dicom.Core/Features/Export/ExportLocation.cs
@@ -0,0 +1,13 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Dicom.Core.Features.Export;
+
+public class ExportLocation
+{
+    public string Type { get; set; }
+
+    public object Configuration { get; set; }
+}
---
+++ src/Microsoft.Health.Dicom.Core/Models/Export/ExportInput.cs
@@ -0,0 +1,18 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Dicom.Core.Features.Export;
+using Microsoft.Health.Dicom.Core.Models.Operations;
+
+namespace Microsoft.Health.Dicom.Core.Models.Export;
+
+public class ExportInput
+{
+    public ExportLocation Source { get; set; }
+
+    public ExportLocation Destination { get; set; }
+
+    public BatchOptions Batching { get; set; }
+}
