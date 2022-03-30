02785c4403d4088c3aa2f747492f2c936f356d55
Will Sugarman <will.sugarman@microsoft.com>
2022-03-30 12:17:06 -0700

    Fix serializers

---
+++ src/Microsoft.Health.Dicom.Functions.UnitTests/Serialization/ConfigurationJsonConverterTests.cs
@@ -0,0 +1,72 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Health.Dicom.Functions.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Xunit;
+
+namespace Microsoft.Health.Dicom.Functions.UnitTests.Serialization;
+
+public class ConfigurationJsonConverterTests
+{
+    private readonly JsonSerializerSettings _serializerSettings;
+
+    public ConfigurationJsonConverterTests()
+    {
+        _serializerSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+        };
+
+        _serializerSettings.Converters.Add(new StringEnumConverter());
+        _serializerSettings.Converters.Add(new ConfigurationJsonConverter());
+    }
+
+    [Fact]
+    public void GivenJson_WhenReading_ThenDeserialize()
+    {
+        const string json = @"{
+  ""word"": ""hello"",
+  ""number"": 42,
+  ""nested"": {
+    ""setting"": ""value""
+  }
+}";
+
+        IConfiguration actual = JsonConvert.DeserializeObject<IConfiguration>(json, _serializerSettings);
+
+        Assert.Equal("hello", actual["word"]);
+        Assert.Equal("42", actual["number"]);
+        Assert.Equal("value", actual["nested:setting"]);
+    }
+
+    [Fact]
+    public void GivenObject_WhenWrite_ThenSerialize()
+    {
+        const string expected = @"{
+  ""nested"": {
+    ""setting"": ""value""
+  },
+  ""number"": ""42"",
+  ""word"": ""hello""
+}";
+
+        IConfiguration value = new ConfigurationBuilder()
+            .AddInMemoryCollection(
+                new KeyValuePair<string, string>[]
+                {
+                    KeyValuePair.Create("word", "hello"),
+                    KeyValuePair.Create("number", "42"),
+                    KeyValuePair.Create("nested:setting", "value"),
+                })
+            .Build();
+
+        string actual = JsonConvert.SerializeObject(value, _serializerSettings);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ src/Microsoft.Health.Dicom.Functions.UnitTests/Serialization/DataSourceJsonConverterTests.cs
@@ -0,0 +1,71 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Microsoft.Health.Dicom.Core.Models.Export;
+using Microsoft.Health.Dicom.Functions.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Xunit;
+
+namespace Microsoft.Health.Dicom.Functions.UnitTests.Serialization;
+
+public class DataSourceJsonConverterTests
+{
+    private readonly JsonSerializerSettings _serializerSettings;
+
+    public DataSourceJsonConverterTests()
+    {
+        _serializerSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.Indented,
+        };
+
+        _serializerSettings.Converters.Add(new StringEnumConverter());
+        _serializerSettings.Converters.Add(new DataSourceJsonConverter());
+    }
+
+    [Fact]
+    public void GivenJson_WhenReading_ThenDeserialize()
+    {
+        const string json = @"{
+  ""Type"": ""UID"",
+  ""Metadata"": [
+    ""foo"",
+    ""bar"",
+    ""baz"",
+  ]
+}";
+
+        DataSource actual = JsonConvert.DeserializeObject<DataSource>(json, _serializerSettings);
+        Assert.Equal(ExportSourceType.UID, actual.Type);
+
+        string[] identifiers = actual.Metadata as string[];
+        Assert.NotNull(identifiers);
+        Assert.True(identifiers.SequenceEqual(new string[] { "foo", "bar", "baz" }));
+    }
+
+    [Fact]
+    public void GivenObject_WhenWrite_ThenSerialize()
+    {
+        const string expected = @"{
+  ""Type"": ""UID"",
+  ""Metadata"": [
+    ""hello"",
+    ""world""
+  ]
+}";
+
+        var value = new DataSource
+        {
+            Metadata = new string[] { "hello", "world" },
+            Type = ExportSourceType.UID,
+        };
+
+        string actual = JsonConvert.SerializeObject(value, _serializerSettings);
+        Assert.Equal(expected, actual);
+    }
+}
