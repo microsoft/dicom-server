1bf7917e9570486724b259600f20bda4c052e2d9
Peng Chen <penche@microsoft.com>
2022-04-13 10:54:56 -0700

    Add InstanceNameBuilder

---
+++ src/Microsoft.Health.Dicom.Core/Features/Store/IInstanceNameBuilder.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Dicom.Core.Features.Model;
+
+namespace Microsoft.Health.Dicom.Core.Features.Store;
+internal interface IInstanceNameBuilder
+{
+    string GetInstanceFileName(VersionedInstanceIdentifier instanceIdentifier);
+
+    string GetInstanceMetadataFileName(VersionedInstanceIdentifier instanceIdentifier);
+}
