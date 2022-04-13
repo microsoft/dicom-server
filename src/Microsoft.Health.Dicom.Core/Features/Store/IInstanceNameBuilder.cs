f63b6d1d09b80039fad668336d0033d515abb660
Peng Chen <penche@microsoft.com>
2022-04-13 15:27:57 -0700

    InstanceNameBuilder

---
+++ src/Microsoft.Health.Dicom.Core/Features/Store/IInstanceNameBuilder.cs
@@ -0,0 +1,19 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Health.Dicom.Core.Features.Model;
+
+namespace Microsoft.Health.Dicom.Core.Features.Store;
+public interface IInstanceNameBuilder
+{
+    string GetInstanceFileName(VersionedInstanceIdentifier instanceIdentifier);
+
+    string GetInstanceMetadataFileName(VersionedInstanceIdentifier instanceIdentifier);
+}
---
+++ src/Microsoft.Health.Dicom.Core/Features/Store/UidAsInstanceName.cs
@@ -0,0 +1,23 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using EnsureThat;
+using Microsoft.Health.Dicom.Core.Features.Model;
+
+namespace Microsoft.Health.Dicom.Core.Features.Store;
+public class UidAsInstanceName : IInstanceNameBuilder
+{
+    public string GetInstanceFileName(VersionedInstanceIdentifier instanceIdentifier)
+    {
+        EnsureArg.IsNotNull(instanceIdentifier, nameof(instanceIdentifier));
+        return $"{instanceIdentifier.StudyInstanceUid}/{instanceIdentifier.SeriesInstanceUid}/{instanceIdentifier.SopInstanceUid}_{instanceIdentifier.Version}.dcm";
+    }
+
+    public string GetInstanceMetadataFileName(VersionedInstanceIdentifier instanceIdentifier)
+    {
+        EnsureArg.IsNotNull(instanceIdentifier, nameof(instanceIdentifier));
+        return $"{instanceIdentifier.StudyInstanceUid}/{instanceIdentifier.SeriesInstanceUid}/{instanceIdentifier.SopInstanceUid}_{instanceIdentifier.Version}_metadata.json";
+    }
+}
