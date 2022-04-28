d6ed41a3c03776b15170ccdfabb7268a2295be76
Peng Chen <penche@microsoft.com>
2022-04-28 13:42:17 -0700

    Add InstanceNameBuilder

---
+++ src/Microsoft.Health.Dicom.Core.UnitTests/Features/Store/WatermarkAsInstanceNameTests.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Dicom.Core.Features.Model;
+using Microsoft.Health.Dicom.Core.Features.Store;
+using Microsoft.Health.Dicom.Tests.Common;
+using Xunit;
+
+namespace Microsoft.Health.Dicom.Core.UnitTests.Features.Store;
+public class WatermarkAsInstanceNameTests
+{
+    private readonly WatermarkAsInstanceName _watearmarkAsInstanceName;
+    public WatermarkAsInstanceNameTests()
+    {
+        _watearmarkAsInstanceName = new WatermarkAsInstanceName();
+    }
+
+    [Fact]
+    public void GivenVersionedInstanceIdentifier_GetNames_ShouldReturnExpectedValues()
+    {
+        VersionedInstanceIdentifier versionedId = new VersionedInstanceIdentifier(TestUidGenerator.Generate(), TestUidGenerator.Generate(), TestUidGenerator.Generate(), 1);
+        Assert.Equal($"{versionedId.Version}.dcm", _watearmarkAsInstanceName.GetInstanceFileName(versionedId));
+        Assert.Equal($"{versionedId.Version}_metadata.json", _watearmarkAsInstanceName.GetInstanceMetadataFileName(versionedId));
+    }
+
+}
---
+++ src/Microsoft.Health.Dicom.Core/Features/Store/IInstanceNameBuilder.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Microsoft.Health.Dicom.Core.Features.Model;
+
+namespace Microsoft.Health.Dicom.Core.Features.Store;
+
+/// <summary>
+/// Provide file name for Dicom instance when store.
+/// </summary>
+public interface IInstanceNameBuilder
+{
+    /// <summary>
+    /// Gets file name for Dicom instance.
+    /// </summary>
+    /// <param name="instanceIdentifier">The Dicom instance identifier.</param>
+    /// <returns>The file name.</returns>
+    string GetInstanceFileName(VersionedInstanceIdentifier instanceIdentifier);
+
+    /// <summary>
+    /// Gets file name for Dicom instance metadata.
+    /// </summary>
+    /// <param name="instanceIdentifier">The Dicom instance identifier.</param>
+    /// <returns>The file name.</returns>
+    string GetInstanceMetadataFileName(VersionedInstanceIdentifier instanceIdentifier);
+}
