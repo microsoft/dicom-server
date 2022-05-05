abc04d15b7eb8bb6854a888426c3f4d9f1f241b9
Will Sugarman <will.sugarman@microsoft.com>
2022-05-04 21:39:11 -0700

    Add new converters and serializers

---
+++ src/Microsoft.Health.Dicom.Core/Extensions/ConfigurationBinderExtensions.cs
@@ -0,0 +1,138 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using EnsureThat;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Health.Dicom.Core.Extensions;
+
+internal static class ConfigurationBinderExtensions
+{
+    public static void Set<T>(this IConfiguration configuration, T value) where T : class
+        => Set(configuration, typeof(T), value, null); // TODO: Avoid boxing?
+
+    public static void Set<T>(this IConfiguration configuration, T value, Action<BinderOptions> configureOptions) where T : class
+        => Set(configuration, typeof(T), value, configureOptions);
+
+    public static void Set(this IConfiguration configuration, Type type, object value)
+        => Set(configuration, type, value, null);
+
+    public static void Set(this IConfiguration configuration, Type type, object value, Action<BinderOptions> configureOptions)
+    {
+        EnsureArg.IsNotNull(configuration, nameof(configuration));
+        EnsureArg.IsNotNull(type, nameof(type));
+        EnsureArg.IsNotNull(value, nameof(value));
+
+        var options = new BinderOptions();
+        configureOptions?.Invoke(options);
+
+        // TODO: Cache a dynamic method to perform the reflection only once. The Get methods are not cached,
+        //       so it is not unreasonable to leave this as-is
+
+        // TODO: Support copying from IConfiguration/IConfigurationSection
+
+        // TODO: Support value types and primitives
+        if (type.IsValueType)
+            throw new InvalidOperationException();
+
+        CopyToConfiguration(configuration, type, value, options);
+    }
+
+    private static void CopyToConfiguration(IConfiguration configuration, Type type, object value, BinderOptions options)
+    {
+        if (value == null)
+            return;
+
+        if (IsLiteralType(type))
+        {
+            if (configuration is not IConfigurationSection section)
+                throw new InvalidOperationException(
+                        string.Format(CultureInfo.CurrentCulture, DicomCoreResource.InvalidTypeBinding, type.Name));
+
+            // Special-case DateTime types to use round-trip
+            if (type == typeof(DateTime) || type == typeof(DateTimeOffset))
+            {
+                section.Value = ((IFormattable)value).ToString("O", CultureInfo.InvariantCulture);
+            }
+            else
+            {
+                TypeConverter converter = TypeDescriptor.GetConverter(type);
+                section.Value = converter.ConvertToInvariantString(value);
+            }
+        }
+        else if (type.IsArray || IsArrayLike(type))
+        {
+            Type elementType;
+            if (type.IsArray)
+            {
+                elementType = type.GetElementType();
+                if (type.IsArray && type.GetArrayRank() > 1)
+                {
+                    throw new InvalidOperationException(
+                        string.Format(CultureInfo.CurrentCulture, DicomCoreResource.InvalidArrayRank, type.GetArrayRank()));
+                }
+            }
+            else
+            {
+                elementType = type.GetGenericArguments()[0];
+            }
+
+            int i = 0;
+            foreach (object element in (IEnumerable)value)
+            {
+                CopyToConfiguration(configuration.GetSection(i.ToString(CultureInfo.InvariantCulture)), elementType, element, options);
+                i++;
+            }
+        }
+        else
+        {
+            BindingFlags bindingFlags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public;
+            if (options.BindNonPublicProperties)
+                bindingFlags |= BindingFlags.NonPublic;
+
+            foreach (PropertyInfo p in type.GetProperties(bindingFlags).Where(x => x.CanWrite && x.CanRead))
+                CopyToConfiguration(configuration.GetSection(p.Name), p.PropertyType, p.GetValue(value), options);
+        }
+    }
+
+    private static bool IsLiteralType(Type type)
+        => type == typeof(bool)
+        || type == typeof(sbyte)
+        || type == typeof(byte)
+        || type == typeof(short)
+        || type == typeof(ushort)
+        || type == typeof(int)
+        || type == typeof(uint)
+        || type == typeof(long)
+        || type == typeof(ulong)
+        || type == typeof(float)
+        || type == typeof(double)
+        || type == typeof(decimal)
+        || type == typeof(char)
+        || type == typeof(string)
+        || type == typeof(DateTime)
+        || type == typeof(DateTimeOffset)
+        || type == typeof(TimeSpan)
+        || type == typeof(Guid)
+        || type == typeof(Uri);
+
+    private static bool IsArrayLike(Type type)
+    {
+        if (!type.IsInterface || !type.IsConstructedGenericType)
+            return false;
+
+        Type genericTypeDefinition = type.GetGenericTypeDefinition();
+        return genericTypeDefinition == typeof(IEnumerable<>)
+            || genericTypeDefinition == typeof(IReadOnlyCollection<>)
+            || genericTypeDefinition == typeof(IReadOnlyList<>);
+    }
+}
--- src/Microsoft.Health.Dicom.Core/Serialization/Newtonsoft/ConfigurationJsonConverter.cs
+++ src/Microsoft.Health.Dicom.Core/Serialization/Newtonsoft/ConfigurationJsonConverter.cs
@@ -5,7 +5,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using EnsureThat;
@@ -34,29 +34,31 @@ internal sealed class ConfigurationJsonConverter : JsonConverter<IConfiguration>
             builder.AddConfiguration(existingValue);
 
         return builder
-            .AddInMemoryCollection(EnumeratePairs(serializer.Deserialize<JObject>(reader)))
+            .AddInMemoryCollection(EnumeratePairs(serializer.Deserialize<JObject>(reader), serializer))
             .Build();
     }
 
     public override void WriteJson(JsonWriter writer, IConfiguration value, JsonSerializer serializer)
         => WriteConfiguration(writer, value);
 
-    private static IEnumerable<KeyValuePair<string, string>> EnumeratePairs(JObject config)
-        => EnumeratePairs(string.Empty, config);
+    private static IEnumerable<KeyValuePair<string, string>> EnumeratePairs(JObject config, JsonSerializer serializer)
+        => EnumeratePairs(string.Empty, config, serializer);
 
-    [SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "Boolean types in JSON are lowercase.")]
-    private static IEnumerable<KeyValuePair<string, string>> EnumeratePairs(string path, JToken token)
+    private static IEnumerable<KeyValuePair<string, string>> EnumeratePairs(string path, JToken token, JsonSerializer serializer)
     {
         if (token is JObject section)
         {
-            foreach (KeyValuePair<string, string> p in section.Properties().SelectMany(x => EnumeratePairs(GetPath(path, x.Name), x.Value)))
+            foreach (KeyValuePair<string, string> p in section.Properties().SelectMany(x => EnumeratePairs(GetPath(path, x.Name), x.Value, serializer)))
             {
                 yield return p;
             }
         }
         else if (token is JArray a)
         {
-            foreach (KeyValuePair<string, string> p in a.Children().SelectMany((x, i) => EnumeratePairs(GetPath(path, i.ToString(CultureInfo.InvariantCulture)), x)))
+            IEnumerable<KeyValuePair<string, string>> elements = a.Children()
+                .SelectMany((x, i) => EnumeratePairs(GetPath(path, i.ToString(CultureInfo.InvariantCulture)), x, serializer));
+
+            foreach (KeyValuePair<string, string> p in elements)
             {
                 yield return p;
             }
@@ -67,18 +69,28 @@ internal sealed class ConfigurationJsonConverter : JsonConverter<IConfiguration>
             {
                 // Newtonsoft by default parses the values and does not provide a means of retrieving the raw string values.
                 // What this ultimately means is that the representation may look different if read as a string from the
-                // configuration or if re-written to JSON (E.g. DateTimes will use the "O" format).
+                // configuration or if re-written to JSON (E.g. DateTimes will use the round trip format).
                 // We could circumvent this by parsing this with JRaw types or manually enumerating the JSON.
                 yield return token.Type switch
                 {
-                    JTokenType.Boolean => KeyValuePair.Create(path, token.Value<bool>().ToString().ToLowerInvariant()), // bool type returns "True"/"False" for ToString()
-                    JTokenType.Date => KeyValuePair.Create(path, token.Value<DateTime>().ToString("O", CultureInfo.InvariantCulture)),
-                    JTokenType.Float => KeyValuePair.Create(path, token.Value<float>().ToString(CultureInfo.InvariantCulture)),
-                    JTokenType.Guid => KeyValuePair.Create(path, token.Value<Guid>().ToString("D", CultureInfo.InvariantCulture)),
-                    JTokenType.Integer => KeyValuePair.Create(path, token.Value<int>().ToString(CultureInfo.InvariantCulture)),
-                    JTokenType.String => KeyValuePair.Create(path, token.Value<string>()),
-                    JTokenType.TimeSpan => KeyValuePair.Create(path, token.Value<TimeSpan>().ToString("c", CultureInfo.InvariantCulture)),
-                    JTokenType.Uri => KeyValuePair.Create(path, token.Value<Uri>().ToString()), // URL may be relative. TODO: Should we try to use AbsoluteUri when possible?
+                    JTokenType.Boolean => KeyValuePair.Create(path, ConvertToString<bool>(value)),
+                    JTokenType.Date =>
+                        KeyValuePair.Create(
+                            path,
+                            serializer.DateParseHandling == DateParseHandling.DateTime
+                                ? value.Value<DateTime>().ToString("O", CultureInfo.InvariantCulture)
+                                : value.Value<DateTimeOffset>().ToString("O", CultureInfo.InvariantCulture)),
+                    JTokenType.Float =>
+                        KeyValuePair.Create(
+                            path,
+                            serializer.FloatParseHandling == FloatParseHandling.Double
+                                ? ConvertToString<double>(value)
+                                : ConvertToString<decimal>(value)),
+                    JTokenType.Guid => KeyValuePair.Create(path, ConvertToString<Guid>(value)),
+                    JTokenType.Integer => KeyValuePair.Create(path, ConvertToString<long>(value)),
+                    JTokenType.String => KeyValuePair.Create(path, value.Value<string>()),
+                    JTokenType.TimeSpan => KeyValuePair.Create(path, ConvertToString<TimeSpan>(value)),
+                    JTokenType.Uri => KeyValuePair.Create(path, ConvertToString<Uri>(value)),
                     _ => throw new JsonException(string.Format(CultureInfo.CurrentCulture, DicomCoreResource.InvalidJsonToken, token.Type))
                 };
             }
@@ -108,4 +120,7 @@ internal sealed class ConfigurationJsonConverter : JsonConverter<IConfiguration>
 
     private static string GetPath(string current, string key)
         => string.IsNullOrEmpty(current) ? key : current + ':' + key;
+
+    private static string ConvertToString<T>(JValue value)
+        => TypeDescriptor.GetConverter(typeof(T)).ConvertToInvariantString(value.Value<T>());
 }
