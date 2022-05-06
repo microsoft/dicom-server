39bf7929a8e4fa20445f62fa698190000c48c242
Peng Chen <67345894+pengchen0692@users.noreply.github.com>
2022-05-06 16:42:45 -0700

    Add Blob Copy orchestration (#1550)
    Add Blob Copy orchestration

---
+++ src/Microsoft.Health.Dicom.Functions.UnitTests/Utils/TaskBatchTests.cs
@@ -0,0 +1,87 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Health.Dicom.Core.Features.Model;
+using Microsoft.Health.Dicom.Functions.Utils;
+using Microsoft.Health.Dicom.Tests.Common;
+using NSubstitute;
+using Xunit;
+
+namespace Microsoft.Health.Dicom.Functions.UnitTests.Utils;
+
+public class TaskBatchTests
+{
+
+    [Fact]
+    public async Task GivenInvalidThreadCount_WhenRunAsync_ThenShouldThrowExceptoin()
+    {
+        VersionedInstanceIdentifier[] ids = RandomIds(5);
+        ITaskCreator taskCreator = Substitute.For<ITaskCreator>();
+        await Assert.ThrowsAsync<ArgumentException>(() => TaskBatch.RunAsync(ids, id => taskCreator.CreateAsync(id), 0));
+    }
+
+    [Fact]
+    public async Task GivenIdsNotDivisibleByThreadCount_WhenRunAsync_ThenShouldSucceed()
+    {
+        VersionedInstanceIdentifier[] ids = RandomIds(5);
+        ITaskCreator taskCreator = Substitute.For<ITaskCreator>();
+        await TaskBatch.RunAsync(ids, id => taskCreator.CreateAsync(id), 2);
+
+        // Assert
+        foreach (var id in ids)
+        {
+            await taskCreator
+                  .Received(1)
+                  .CreateAsync(id);
+        }
+    }
+
+    [Fact]
+    public async Task GivenIdsDivisibleByThreadCount_WhenRunAsync_ThenShouldSucceed()
+    {
+        VersionedInstanceIdentifier[] ids = RandomIds(6);
+        ITaskCreator taskCreator = Substitute.For<ITaskCreator>();
+        await TaskBatch.RunAsync(ids, id => taskCreator.CreateAsync(id), 2);
+
+        // Assert
+        foreach (var id in ids)
+        {
+            await taskCreator
+                  .Received(1)
+                  .CreateAsync(id);
+        }
+    }
+
+    [Fact]
+    public async Task GivenNoIds_WhenRunAsync_ThenShouldNotCallTaskCreator()
+    {
+        VersionedInstanceIdentifier[] ids = RandomIds(0);
+        ITaskCreator taskCreator = Substitute.For<ITaskCreator>();
+        await TaskBatch.RunAsync(ids, id => taskCreator.CreateAsync(id), 2);
+
+        // Assert
+        await taskCreator
+              .DidNotReceive()
+              .CreateAsync(Arg.Any<VersionedInstanceIdentifier>());
+
+    }
+
+    private static VersionedInstanceIdentifier[] RandomIds(int count)
+    {
+        var ids = new VersionedInstanceIdentifier[count];
+        for (int i = 0; i < count; i++)
+        {
+            ids[i] = new VersionedInstanceIdentifier(TestUidGenerator.Generate(), TestUidGenerator.Generate(), TestUidGenerator.Generate(), i);
+        }
+        return ids;
+    }
+
+    public interface ITaskCreator
+    {
+        Task CreateAsync(VersionedInstanceIdentifier id);
+    }
+}
---
+++ src/Microsoft.Health.Dicom.Functions/Utils/TaskBatch.cs
@@ -0,0 +1,47 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnsureThat;
+
+namespace Microsoft.Health.Dicom.Functions.Utils;
+
+internal static class TaskBatch
+{
+    /// <summary>
+    /// Run a batch of batch and wait for completion.
+    /// </summary>
+    /// <typeparam name="T">The input type.</typeparam>
+    /// <param name="input">The input</param>
+    /// <param name="taskFactory">The task factory.</param>
+    /// <param name="threadCount">The max thread count. -1 means no limit.</param>
+    /// <returns>The task.</returns>
+    public static async Task RunAsync<T>(IReadOnlyList<T> input, Func<T, Task> taskFactory, int threadCount = -1)
+    {
+        EnsureArg.IsNotNull(input, nameof(input));
+        EnsureArg.IsNot(threadCount, 0, nameof(threadCount));
+        EnsureArg.IsNotNull(taskFactory);
+        if (threadCount < 0)
+        {
+            await Task.WhenAll(input.Select(x => taskFactory(x)));
+        }
+        else
+        {
+            for (int i = 0; i < input.Count; i += threadCount)
+            {
+                var tasks = new List<Task>();
+                for (int j = i; j < Math.Min(input.Count, i + threadCount); j++)
+                {
+                    tasks.Add(taskFactory(input[j]));
+                }
+
+                await Task.WhenAll(tasks);
+            }
+        }
+    }
+}
