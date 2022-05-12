5f09d3bf169dd86fca2063be74191b62a73c1c9a
Nick Maliwacki <nmaliwa@microsoft.com>
2022-05-12 16:34:51 -0700

    fix space

--- converter/dicom-cast/src/Microsoft.Health.DicomCast.Core/Modules/DicomModule.cs
+++ converter/dicom-cast/src/Microsoft.Health.DicomCast.Core/Modules/DicomModule.cs
@@ -40,8 +40,8 @@ public class DicomModule : IStartupModule
 
         // Allow retries to occur catch 30 second outages
         var retryPolicy = HttpPolicyExtensions
-                .HandleTransientHttpError() // HttpRequestException, 5XX and 408
-                .WaitAndRetryAsync(8, retryAttempt => retryAttempt <= 3 ? TimeSpan.FromSeconds(retryAttempt) : TimeSpan.FromSeconds(5));
+            .HandleTransientHttpError() // HttpRequestException, 5XX and 408
+            .WaitAndRetryAsync(8, retryAttempt => retryAttempt <= 3 ? TimeSpan.FromSeconds(retryAttempt) : TimeSpan.FromSeconds(5));
 
         services.AddHttpClient<IDicomWebClient, DicomWebClient>((sp, client) =>
             {
