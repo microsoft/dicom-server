e528f1f0011b5526112340ffc4ef70e4d30d8809
Shiva Kumar <v-shivakumar@microsoft.com>
2022-05-26 13:51:46 -0400

    Added parameter assert

--- src/Microsoft.Health.Dicom.Core/Features/Workitem/WorkitemService.ChangeState.cs
+++ src/Microsoft.Health.Dicom.Core/Features/Workitem/WorkitemService.ChangeState.cs
@@ -29,6 +29,7 @@ public partial class WorkitemService
         CancellationToken cancellationToken = default)
     {
         EnsureArg.IsNotNull(dataset, nameof(dataset));
+        EnsureArg.IsNotEmptyOrWhiteSpace(workitemInstanceUid, nameof(workitemInstanceUid));
 
         try
         {
