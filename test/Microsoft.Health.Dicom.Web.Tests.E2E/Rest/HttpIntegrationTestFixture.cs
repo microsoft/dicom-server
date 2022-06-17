5c77bad1da104bf63768239a24e2043442905177
Karthik Balasubramanian <kabalas@microsoft.com>
2022-06-16 22:29:09 -0500

    Add E2E Inproc tests to validate dualwrite STOW scenario (#1698)
    * Revert "Change to IO.Hashing"
    
    This reverts commit 795d2d5854e4975b59c557a47cada34a3baeeae0.
    
    * Add Inproc E2E Tests for dual write scenario

--- test/Microsoft.Health.Dicom.Web.Tests.E2E/Rest/HttpIntegrationTestFixture.cs
+++ test/Microsoft.Health.Dicom.Web.Tests.E2E/Rest/HttpIntegrationTestFixture.cs
@@ -21,12 +21,12 @@ public class HttpIntegrationTestFixture<TStartup> : IDisposable
     private readonly Dictionary<(string, string), AuthenticationHttpMessageHandler> _authenticationHandlers = new Dictionary<(string, string), AuthenticationHttpMessageHandler>();
 
     public HttpIntegrationTestFixture()
-        : this(enableDataPartitions: false)
+        : this(enableDataPartitions: false, enableDualWrite: false)
     { }
 
-    protected HttpIntegrationTestFixture(bool enableDataPartitions)
+    protected HttpIntegrationTestFixture(bool enableDataPartitions, bool enableDualWrite = false)
     {
-        TestDicomWebServer = TestDicomWebServerFactory.GetTestDicomWebServer(typeof(TStartup), enableDataPartitions);
+        TestDicomWebServer = TestDicomWebServerFactory.GetTestDicomWebServer(typeof(TStartup), enableDataPartitions, enableDualWrite);
     }
 
     public bool IsInProcess => TestDicomWebServer is InProcTestDicomWebServer;
