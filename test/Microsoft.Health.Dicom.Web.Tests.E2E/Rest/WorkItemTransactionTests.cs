2bd18a04cd670cb8dba29a9c1dba95a00b6a18ee
Shiva Kumar <v-shivakumar@microsoft.com>
2022-07-11 17:28:09 -0400

    Updated the workitem e2e tests to use DataPartition test fixture

--- test/Microsoft.Health.Dicom.Web.Tests.E2E/Rest/WorkItemTransactionTests.cs
+++ test/Microsoft.Health.Dicom.Web.Tests.E2E/Rest/WorkItemTransactionTests.cs
@@ -15,12 +15,12 @@ using Xunit;
 
 namespace Microsoft.Health.Dicom.Web.Tests.E2E.Rest;
 
-public partial class WorkItemTransactionTests : IClassFixture<HttpIntegrationTestFixture<Startup>>
+public partial class WorkItemTransactionTests : IClassFixture<DataPartitionEnabledHttpIntegrationTestFixture<Startup>>
 {
     private readonly IDicomWebClient _client;
-    private readonly HttpIntegrationTestFixture<Startup> _fixture;
+    private readonly DataPartitionEnabledHttpIntegrationTestFixture<Startup> _fixture;
 
-    public WorkItemTransactionTests(HttpIntegrationTestFixture<Startup> fixture)
+    public WorkItemTransactionTests(DataPartitionEnabledHttpIntegrationTestFixture<Startup> fixture)
     {
         EnsureArg.IsNotNull(fixture, nameof(fixture));
         _fixture = fixture;
