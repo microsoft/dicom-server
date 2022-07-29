88f9f084eb086d553a2b58e2598a253d63abfcb3
Pooja Adhikari <poadhika@microsoft.com>
2022-07-29 10:00:56 -0500

    Added new telemetry to dicomcast

--- converter/dicom-cast/src/Microsoft.Health.DicomCast.Core/Features/Worker/DicomCastWorker.cs
+++ converter/dicom-cast/src/Microsoft.Health.DicomCast.Core/Features/Worker/DicomCastWorker.cs
@@ -4,13 +4,18 @@
 // -------------------------------------------------------------------------------------------------
 
 using System;
+using System.Net;
 using System.Threading;
+using Azure.Identity;
 using EnsureThat;
+using Microsoft.ApplicationInsights;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Health.Dicom.Client;
 using Microsoft.Health.DicomCast.Core.Configurations;
 using Microsoft.Health.DicomCast.Core.Features.Fhir;
+using Microsoft.Health.Fhir.Client;
 using Task = System.Threading.Tasks.Task;
 
 namespace Microsoft.Health.DicomCast.Core.Features.Worker;
@@ -49,25 +54,29 @@ public class DicomCastWorker : IDicomCastWorker
     private readonly ILogger _logger;
     private readonly IHostApplicationLifetime _hostApplicationLifetime;
     private readonly IFhirService _fhirService;
+    private readonly TelemetryClient _telemetryClient;
 
     public DicomCastWorker(
         IOptions<DicomCastWorkerConfiguration> dicomCastWorkerConfiguration,
         IChangeFeedProcessor changeFeedProcessor,
         ILogger<DicomCastWorker> logger,
         IHostApplicationLifetime hostApplicationLifetime,
-        IFhirService fhirService)
+        IFhirService fhirService,
+        TelemetryClient telemetryClient)
     {
         EnsureArg.IsNotNull(dicomCastWorkerConfiguration?.Value, nameof(dicomCastWorkerConfiguration));
         EnsureArg.IsNotNull(changeFeedProcessor, nameof(changeFeedProcessor));
         EnsureArg.IsNotNull(logger, nameof(logger));
         EnsureArg.IsNotNull(hostApplicationLifetime, nameof(hostApplicationLifetime));
         EnsureArg.IsNotNull(fhirService, nameof(fhirService));
+        EnsureArg.IsNotNull(telemetryClient, nameof(telemetryClient));
 
         _dicomCastWorkerConfiguration = dicomCastWorkerConfiguration.Value;
         _changeFeedProcessor = changeFeedProcessor;
         _logger = logger;
         _hostApplicationLifetime = hostApplicationLifetime;
         _fhirService = fhirService;
+        _telemetryClient = telemetryClient;
     }
 
     /// <inheritdoc/>
@@ -99,6 +108,23 @@ public class DicomCastWorker : IDicomCastWorker
         catch (Exception ex)
         {
             LogUnhandledExceptionDelegate(_logger, ex);
+
+            if ((ex is FhirException) && ((FhirException)ex).StatusCode == HttpStatusCode.Forbidden)
+            {
+                _telemetryClient.GetMetric("cast-to-fhir-forbidden").TrackValue(1);
+            }
+            else if ((ex is DicomWebException) && ((DicomWebException)ex).StatusCode == HttpStatusCode.Forbidden)
+            {
+                _telemetryClient.GetMetric("dicom-to-cast-forbidden").TrackValue(1);
+            }
+            else if (ex is CredentialUnavailableException)
+            {
+                _telemetryClient.GetMetric("cast-mi-unavailable").TrackValue(1);
+            }
+            else
+            {
+                _telemetryClient.GetMetric("cast-failed").TrackValue(1);
+            }
 
             // Any exception in ExecuteAsync will not shutdown application, call hostApplicationLifetime.StopApplication() to force shutdown.
             // Please refer to .net core issue on github for more details: "Exceptions in BackgroundService ExecuteAsync are (sometimes) hidden" https://github.com/dotnet/extensions/issues/2363
