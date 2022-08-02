57676905f4867aa1c43ba6cfbf5dd22a68045c0c
Esma Dautovic <esmada@microsoft.com>
2022-08-02 15:44:30 -0700

    compare against YAML on main

--- build/common/scripts/CheckForBreakingAPISwaggerChanges.ps1
+++ build/common/scripts/CheckForBreakingAPISwaggerChanges.ps1
@@ -23,12 +23,22 @@ param(
 $ErrorActionPreference = 'Stop'
 $container="openapitools/openapi-diff:latest@sha256:5da8291d3947414491e4c62de74f8fc1ee573a88461fb2fb09979ecb5ea5eb02"
 
+mkdir "$SwaggerDir/FromMain"
+
 foreach ($Version in $Versions)
 {
-    $old=(Join-Path -Path "$SwaggerDir" -ChildPath "$Version/swagger.yaml")
-    $new="/$SwaggerDir/$Version.yaml"
+    $new=(Join-Path -Path "$SwaggerDir" -ChildPath "$Version/swagger.yaml")
+    $old=(Join-Path -Path "$SwaggerDir" -ChildPath "/FromMain/$Version.yaml")
+
+    $SwaggerOnMain="https://raw.githubusercontent.com/microsoft/dicom-server/main/swagger/$Version/swagger.yaml"
+    Invoke-WebRequest -Uri $SwaggerOnMain -OutFile $old
+
+
     write-host "Running comparison with baseline for version $Version"
     Write-Host "old: $old"
     Write-Host "new: $new"
-    docker run --rm -t -v "${pwd}/${SwaggerDir}:/swagger:ro" $container /$old $new --fail-on-incompatible
+    docker run --rm -t -v "${pwd}/${SwaggerDir}:/swagger:ro" $container /$old /$new --fail-on-incompatible
+    rm $old
 }
+
+rmdir "$SwaggerDir/FromMain"
