4ed243c34936f1178d502d5b2a764528ddd0142a
Esma Dautovic <esmada@microsoft.com>
2022-08-02 11:59:20 -0700

    See if we can reuse swagger generated from previous task

---
+++ build/common/scripts/CheckForBreakingAPISwaggerChanges.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+Generates the OpenApi doc for the specified version and compares it with the baseline to make sure no breaking changes are introduced
+.Parameter SwaggerDir
+The working directory
+.PARAMETER AssemblyDir
+Path for the web projects dll
+.PARAMETER Versions
+Api versions to generate the OpenApiDoc for and compare with baseline
+.PARAMETER SwashbuckleCLIVersion
+Version of SwashbuckleCLI to use
+#>
+
+param(
+    [string]$SwaggerDir,
+
+    [string]$AssemblyDir,
+
+    [String[]]$Versions,
+
+    [string]$SwashbuckleCLIVersion = '6.4.0'
+)
+$ErrorActionPreference = 'Stop'
+
+ls "$SwaggerDir"
