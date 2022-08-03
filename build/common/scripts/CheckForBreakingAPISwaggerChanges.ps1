59ed5142cb525e76dc9b4cf28a008447c30d5487
Esma Dautovic <esmada@microsoft.com>
2022-08-02 17:10:17 -0700

    do not need assembly path

--- build/common/scripts/CheckForBreakingAPISwaggerChanges.ps1
+++ build/common/scripts/CheckForBreakingAPISwaggerChanges.ps1
@@ -3,8 +3,6 @@
 Generates the OpenApi doc for the specified version and compares it with the baseline to make sure no breaking changes are introduced
 .Parameter SwaggerDir
 The working directory
-.PARAMETER AssemblyDir
-Path for the web projects dll
 .PARAMETER Versions
 Api versions to generate the OpenApiDoc for and compare with baseline
 .PARAMETER SwashbuckleCLIVersion
@@ -13,8 +11,6 @@ Version of SwashbuckleCLI to use
 
 param(
     [string]$SwaggerDir,
-
-    [string]$AssemblyDir,
 
     [String[]]$Versions,
 
