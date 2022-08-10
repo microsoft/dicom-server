99b288e69ee38487ce144b808f88f47edb50f3a1
lagomesma <69813504+lagomesma@users.noreply.github.com>
2022-08-10 09:12:04 -0700

    update swagger on build (#1835)
    * Generate swagger on web build.
    
    * Check for latest Swagger changes and fail pipeline when changes detected indicating author needs to add updated swagger files to PR.
    
    * Use dotnet tool restore to get swagger/swashbuckle installed

---
+++ build/common/scripts/CheckForSwaggerChanges.ps1
@@ -0,0 +1,46 @@
+﻿<#
+.SYNOPSIS
+Generates the OpenApi doc for the specified version and compares it with the checked in version to ensure it is up to date.
+.Parameter SwaggerDir
+The working directory
+.PARAMETER AssemblyDir
+Path for the web projects dll
+.PARAMETER Versions
+Api versions to compare with
+.PARAMETER SwashbuckleCLIVersion
+Version of SwashbuckleCLI to use
+#>
+
+param(
+    [string]$SwaggerDir,
+
+    [string]$AssemblyDir,
+
+    [String[]]$Versions
+)
+
+$ErrorActionPreference = 'Stop' # ensure script behaves same locally as within default pwsh ado task
+dotnet tool restore
+
+Write-Host "Using swagger version ..."
+dotnet tool list | Select-String "swashbuckle"
+
+Write-Host "Testing that swagger will work ..."
+dotnet swagger
+
+foreach ($Version in $Versions)
+{
+    Write-Host "Generating swagger yaml file for $Version"
+    $WritePath=(Join-Path -Path "$SwaggerDir" -ChildPath "$Version.yaml")
+
+    dotnet swagger tofile --yaml --output $WritePath "$AssemblyDir" $Version
+
+    Write-Host "Comparing generated swagger with what was checked in ..."
+    $HasDifferences = (Compare-Object -ReferenceObject (Get-Content -Path $WritePath) -DifferenceObject (Get-Content -Path ".\swagger\$Version\swagger.yaml"))
+    if ($HasDifferences){
+        Write-Host $HasDifferences
+        throw "The swagger yaml checked in with this PR is not up to date with code. Please build the sln, which will trigger a hook to autogenerate these files on your behalf. Differences shown above."
+    } else{
+        Write-Host "Swagger checked in with this PR is up to date with code."
+    }
+}
