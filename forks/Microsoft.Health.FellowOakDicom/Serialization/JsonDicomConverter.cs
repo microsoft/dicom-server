2b71db5511ee5e568d8291cdb965a63650da47fc
Shiva Kumar <94023192+shivakumar-ms@users.noreply.github.com>
2022-09-15 14:49:37 -0400

    [AB#93875] Fixed deserialization issue with DS, IS, SV and UV VR types with non-numeric string values. (#1943)
    [AB#93875]
    * Forked fo-dicom JsonConverter locally to fix deserialization issue
    * Fixed deserialization issue with DS, IS, SV and UV VR types
    * Refactored fo-dicom DS, IS, SV and UV Element types. Added DicomMultiStringOrNumberElement base class.

--- forks/Microsoft.Health.FellowOakDicom/Serialization/JsonDicomConverter.cs
+++ forks/Microsoft.Health.FellowOakDicom/Serialization/JsonDicomConverter.cs
@@ -11,7 +11,14 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Text.RegularExpressions;
 using FellowOakDicom;
+using FellowOakDicom.IO;
 using FellowOakDicom.IO.Buffer;
+
+using DicomDecimalString = Microsoft.Health.FellowOakDicom.Core.DicomDecimalString;
+using DicomIntegerString = Microsoft.Health.FellowOakDicom.Core.DicomIntegerString;
+using DicomSignedVeryLong = Microsoft.Health.FellowOakDicom.Core.DicomSignedVeryLong;
+using DicomUnsignedVeryLong = Microsoft.Health.FellowOakDicom.Core.DicomUnsignedVeryLong;
+
 
 namespace Microsoft.Health.FellowOakDicom.Serialization
 {
@@ -862,7 +869,7 @@ namespace Microsoft.Health.FellowOakDicom.Serialization
             }
         }
 
-        private static T[] ReadJsonMultiNumberOrStringValue<T>(ref Utf8JsonReader reader, GetValue<T> getValue, TryParse<T> tryParse)
+        private object ReadJsonMultiNumberOrStringValue<T>(ref Utf8JsonReader reader, GetValue<T> getValue, TryParse<T> tryParse)
         {
             if (reader.TokenType == JsonTokenType.Null)
             {
@@ -871,7 +878,8 @@ namespace Microsoft.Health.FellowOakDicom.Serialization
             }
             reader.AssumeAndSkip(JsonTokenType.StartArray);
 
-            var childValues = new List<T>();
+            var hasNonNumericString = false;
+            var childValues = new List<object>();
             while (reader.TokenType != JsonTokenType.EndArray)
             {
                 if (reader.TokenType == JsonTokenType.Number)
@@ -886,6 +894,11 @@ namespace Microsoft.Health.FellowOakDicom.Serialization
                 {
                     childValues.Add(parsed);
                 }
+                else if (reader.TokenType == JsonTokenType.String && !_autoValidate)
+                {
+                    hasNonNumericString = true;
+                    childValues.Add(reader.GetString());
+                }
                 else
                 {
                     throw new JsonException("Malformed DICOM json, number expected");
@@ -894,8 +907,13 @@ namespace Microsoft.Health.FellowOakDicom.Serialization
             }
             reader.AssumeAndSkip(JsonTokenType.EndArray);
 
-            var data = childValues.ToArray();
-            return data;
+            if (hasNonNumericString)
+            {
+                var valArray = childValues.Select(x => x.ToString()).ToArray();
+                return ByteConverter.ToByteBuffer(string.Join("\\", valArray));
+            }
+
+            return childValues.Select(x => x).Cast<T>().ToArray();
         }
 
         private object ReadJsonMultiNumber<T>(ref Utf8JsonReader reader, GetValue<T> getValue)
