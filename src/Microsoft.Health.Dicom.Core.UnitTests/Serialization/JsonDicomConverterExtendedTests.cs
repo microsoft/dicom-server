1ab1cb3f9c196d1b65f7a4f0baa664d84968d41b
Esma Dautovic <esmada@microsoft.com>
2022-10-24 14:55:13 -0700

    Revert "use realistic data for test"
    This reverts commit 8c37f46cced908fb13dfa5cc747c02d98e025e63.

--- src/Microsoft.Health.Dicom.Core.UnitTests/Serialization/JsonDicomConverterExtendedTests.cs
+++ src/Microsoft.Health.Dicom.Core.UnitTests/Serialization/JsonDicomConverterExtendedTests.cs
@@ -149,17 +149,15 @@ public class JsonDicomConverterExtendedTests
 
 
     [Fact]
-    public void GivenDicomJsonDatasetWithDSVRContainsNotNANOrNumericValue_WhenDeserialized_ThrowsException()
+    public void GivenDicomJsonDatasetWithFloatingVRContainsNotNANOrNumericValue_WhenDeserialized_ThrowsException()
     {
         // Some Dicom values are meant to be numeric such as FL. You can represent a null value with
         // "NaN" string, but otherwise only numerical values are accepted
-        // 00200032 is Image Position (Patient) Attribute, the x, y, and z coordinates of the upper left hand corner
-        // of the image
         var json = @"
             {
-                ""00200032"": {
-                    ""vr"":""DS"",
-                    ""Value"":[265.000, ""NaN"", ""BADVALUE""]
+                ""00101030"": {
+                    ""vr"":""FL"",
+                    ""Value"":[""BADVALUE""]
                 }
             }";
 
