68b802fa06c30db41bc7f7fc864946c724a6c7c3
Karthik Balasubramanian <karthikb87@gmail.com>
2022-12-29 13:56:31 -0600

    Revert "Revert "Revert "Bump dotnet/sdk from 7.0.100-alpine3.16 to 7.0.101-alpine3.16"" (#2215)"
    This reverts commit 22ab38674ac840797c767df0600639511c918985.

--- src/Microsoft.Health.Dicom.Core/Features/Retrieve/ETagGenerator.cs
+++ src/Microsoft.Health.Dicom.Core/Features/Retrieve/ETagGenerator.cs
@@ -1,4 +1,4 @@
-// -------------------------------------------------------------------------------------------------
+﻿// -------------------------------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
@@ -14,14 +14,15 @@ namespace Microsoft.Health.Dicom.Core.Features.Retrieve;
 
 public class ETagGenerator : IETagGenerator
 {
-    public string GetETag(ResourceType resourceType, IReadOnlyCollection<VersionedInstanceIdentifier> retrieveInstances)
+    public string GetETag(ResourceType resourceType, IEnumerable<VersionedInstanceIdentifier> retrieveInstances)
     {
         EnsureArg.IsTrue(
             resourceType == ResourceType.Study ||
             resourceType == ResourceType.Series ||
             resourceType == ResourceType.Instance,
             nameof(resourceType));
-        EnsureArg.HasItems(retrieveInstances);
+        EnsureArg.IsNotNull(retrieveInstances);
+        EnsureArg.IsTrue(retrieveInstances.Any());
 
         string eTag = string.Empty;
         long maxWatermark = retrieveInstances.Max(ri => ri.Version);
@@ -30,7 +31,7 @@ public class ETagGenerator : IETagGenerator
         {
             case ResourceType.Study:
             case ResourceType.Series:
-                int countInstances = retrieveInstances.Count;
+                int countInstances = retrieveInstances.Count();
                 eTag = $"{maxWatermark}-{countInstances}";
                 break;
             case ResourceType.Instance:
