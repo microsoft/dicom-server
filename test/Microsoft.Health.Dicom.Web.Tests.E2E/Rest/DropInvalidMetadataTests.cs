23db491572c24eb48e6c65c9d539c2ceea84ef1f
Esma Dautovic <esmada@microsoft.com>
2023-02-06 11:14:22 -0800

    remove bvt trait and use a single e2e test for leniency

--- test/Microsoft.Health.Dicom.Web.Tests.E2E/Rest/DropInvalidMetadataTests.cs
+++ test/Microsoft.Health.Dicom.Web.Tests.E2E/Rest/DropInvalidMetadataTests.cs
@@ -33,8 +33,7 @@ public class DropInvalidMetadataTests : IClassFixture<EnableDropInvalidDicomJson
     }
 
     [Fact]
-    [Trait("Category", "bvt-leniency")]
-    public async Task GivenInstanceWithAnInvalidIndexableAttribute_WhenEnableDropInvalidDicomJsonMetadata_ThenValidDataStillWritten()
+    public async Task GivenInstanceWithAnInvalidIndexableAttribute_WhenEnableDropInvalidDicomJsonMetadata_ThenInvalidDataDroppedAndValidDataWritten()
     {
         // setup
         DicomFile dicomFile = GenerateDicomFile();
@@ -47,11 +46,11 @@ public class DropInvalidMetadataTests : IClassFixture<EnableDropInvalidDicomJson
         dicomFile.Dataset.Add(dicomDataset);
 
         // run
-        await _instancesManager.StoreAsync(
+        DicomWebResponse<DicomDataset> response = await _instancesManager.StoreAsync(
             new[] { dicomFile },
             partitionName: _partition);
 
-        // assert
+        // assertions
 
         using DicomWebResponse<DicomFile> retrievedInstance = await _client.RetrieveInstanceAsync(
             dicomFile.Dataset.GetString(DicomTag.StudyInstanceUID),
@@ -75,44 +74,6 @@ public class DropInvalidMetadataTests : IClassFixture<EnableDropInvalidDicomJson
 
         // valid searchable index attr was stored, so we can query for instance using the valid attr
         Assert.Single(await GetInstanceByAttribute(dicomFile, DicomTag.PatientBirthDate));
-    }
-
-    [Fact]
-    public async Task GivenInstanceWithAnInvalidIndexableAttribute_WhenEnableDropInvalidDicomJsonMetadata_ThenInvalidDataDropped()
-    {
-        // setup
-        DicomFile dicomFile = GenerateDicomFile();
-
-        DicomDataset dicomDataset = new DicomDataset().NotValidated();
-
-        dicomDataset.Add(DicomTag.StudyDate, "NotAValidStudyDate");
-        dicomDataset.Add(DicomTag.PatientBirthDate, "20220315");
-
-        dicomFile.Dataset.Add(dicomDataset);
-
-        // run
-        await _instancesManager.StoreAsync(
-            new[] { dicomFile },
-            partitionName: _partition);
-
-        // assert
-
-        using DicomWebResponse<DicomFile> retrievedInstance = await _client.RetrieveInstanceAsync(
-            dicomFile.Dataset.GetString(DicomTag.StudyInstanceUID),
-            dicomFile.Dataset.GetString(DicomTag.SeriesInstanceUID),
-            dicomFile.Dataset.GetString(DicomTag.SOPInstanceUID),
-            dicomTransferSyntax: "*",
-            partitionName: _partition);
-
-        DicomFile retrievedDicomFile = await retrievedInstance.GetValueAsync();
-
-        // expect that invalid attribute stored in dicom file
-        Assert.Equal(
-            dicomFile.Dataset.GetString(DicomTag.StudyDate),
-            retrievedDicomFile.Dataset.GetString(DicomTag.StudyDate)
-            );
-
-        DicomDataset retrievedMetadata = await ResponseHelper.GetMetadata(_client, dicomFile, _partition);
 
         // expect that metadata invalid date not present
         DicomDataException thrownException = Assert.Throws<DicomDataException>(
@@ -127,29 +88,9 @@ public class DropInvalidMetadataTests : IClassFixture<EnableDropInvalidDicomJson
             "BadRequest: Invalid query: specified Date value 'NotAValidStudyDate' is invalid for attribute 'StudyDate'" +
             ". Date should be valid and formatted as yyyyMMdd.",
             caughtException.Message);
-    }
 
-    [Fact]
-    public async Task GivenInstanceWithAnInvalidIndexableAttribute_WhenEnableDropInvalidDicomJsonMetadata_ThenExpectASingleCommentsSequenceInResponse()
-    {
-        // setup
-        DicomFile dicomFile = GenerateDicomFile();
-
-        DicomDataset dicomDataset = new DicomDataset().NotValidated();
-
-        dicomDataset.Add(DicomTag.StudyDate, "NotAValidStudyDate");
-        dicomDataset.Add(DicomTag.PatientBirthDate, "20220315");
-
-        dicomFile.Dataset.Add(dicomDataset);
-
-        // run
-        DicomWebResponse<DicomDataset> response = await _instancesManager.StoreAsync(
-            new[] { dicomFile },
-            partitionName: _partition);
-
+        // assert on response
         DicomDataset responseDataset = await response.GetValueAsync();
-
-        // assert
         DicomSequence refSopSequence = responseDataset.GetSequence(DicomTag.ReferencedSOPSequence);
         Assert.Single(refSopSequence);
 
@@ -163,126 +104,7 @@ public class DropInvalidMetadataTests : IClassFixture<EnableDropInvalidDicomJson
         Assert.Equal(
             """DICOM100: (0008,0020) - Content "NotAValidStudyDate" does not validate VR DA: one of the date values does not match the pattern YYYYMMDD""",
             failedAttributesSequence.Items[0].GetString(DicomTag.ErrorComment)
-            );
-    }
-
-    [Fact]
-    public async Task GivenInstanceWithMultipleInvalidIndexableAttributes_WhenEnableDropInvalidDicomJsonMetadata_ThenExpectMultipleErrorCommentsInASingleCommentsSequenceInResponse()
-    {
-        // setup
-        DicomFile dicomFile = GenerateDicomFile();
-
-        DicomDataset dicomDataset = new DicomDataset().NotValidated();
-
-        dicomDataset.Add(DicomTag.StudyDate, "NotAValidStudyDate");
-        dicomDataset.Add(DicomTag.PatientBirthDate, "NotAValidStudyDate");
-
-        dicomFile.Dataset.Add(dicomDataset);
-
-        // run
-        DicomWebResponse<DicomDataset> response = await _instancesManager.StoreAsync(
-            new[] { dicomFile },
-            partitionName: _partition);
-        DicomDataset responseDataset = await response.GetValueAsync();
-
-        // assert
-        DicomSequence refSopSequence = responseDataset.GetSequence(DicomTag.ReferencedSOPSequence);
-        Assert.Single(refSopSequence);
-
-        DicomDataset firstInstance = refSopSequence.Items[0];
-
-        // expect a comment sequence present
-        DicomSequence failedAttributesSequence = firstInstance.GetSequence(DicomTag.FailedAttributesSequence);
-
-        // expect comment sequence has same count of warnings as invalid attributes
-        Assert.Equal(2, failedAttributesSequence.Items.Count);
-
-        // expect that the two invalid attributes are represented in warnings
-        Assert.Equal(
-            """DICOM100: (0008,0020) - Content "NotAValidStudyDate" does not validate VR DA: one of the date values does not match the pattern YYYYMMDD""",
-            failedAttributesSequence.Items[0].GetString(DicomTag.ErrorComment));
-
-        // expect that the two invalid attributes are represented in warnings
-        Assert.Equal(
-            """DICOM100: (0010,0030) - Content "NotAValidStudyDate" does not validate VR DA: one of the date values does not match the pattern YYYYMMDD""",
-            failedAttributesSequence.Items[1].GetString(DicomTag.ErrorComment)
         );
-    }
-
-    [Fact]
-    public async Task GivenMultipleInstancesWithInvalidIndexableAttributes_WhenEnableDropInvalidDicomJsonMetadata_ThenExpectMultipleCommentsSequencesInResponse()
-    {
-        // setup
-        DicomFile dicomFile1 = GenerateDicomFile();
-
-        DicomFile dicomFile2 = GenerateDicomFile();
-
-        DicomDataset dicomDataset = new DicomDataset().NotValidated();
-
-        dicomDataset.Add(DicomTag.StudyDate, "NotAValidStudyDate");
-
-        dicomFile1.Dataset.Add(dicomDataset);
-        dicomFile2.Dataset.Add(dicomDataset);
-
-        // run
-        DicomWebResponse<DicomDataset> response = await _instancesManager.StoreAsync(
-            new[] { dicomFile1, dicomFile2 },
-            partitionName: _partition);
-        DicomDataset responseDataset = await response.GetValueAsync();
-
-        // assert
-        DicomSequence refSopSequence = responseDataset.GetSequence(DicomTag.ReferencedSOPSequence);
-
-        // assert the refSopSequence has two instances being represented
-        Assert.Equal(2, refSopSequence.Items.Count);
-
-        foreach (DicomDataset instance in refSopSequence.Items)
-        {
-            // expect a comment sequence present
-            DicomSequence failedAttributesSequence = instance.GetSequence(DicomTag.FailedAttributesSequence);
-
-            // expect comment sequence has same count of warnings as invalid attributes
-            Assert.Single(failedAttributesSequence);
-
-            // expect that the two invalid attribute is represented in warnings
-            Assert.Equal(
-                """DICOM100: (0008,0020) - Content "NotAValidStudyDate" does not validate VR DA: one of the date values does not match the pattern YYYYMMDD""",
-                failedAttributesSequence.Items[0].GetString(DicomTag.ErrorComment));
-        }
-    }
-
-    [Fact]
-    public async Task GivenInstanceWithAValidIndexableAttribute_WhenEnableDropInvalidDicomJsonMetadata_ThenExpectEmptyFailedAttributesSequence()
-    {
-        // setup
-        DicomFile dicomFile = GenerateDicomFile();
-
-        DicomDataset dicomDataset = new DicomDataset().NotValidated();
-
-        dicomDataset.Add(DicomTag.PatientBirthDate, "20220315");
-
-        dicomFile.Dataset.Add(dicomDataset);
-
-        // run
-        DicomWebResponse<DicomDataset> response = await _instancesManager.StoreAsync(
-            new[] { dicomFile },
-            partitionName: _partition);
-        DicomDataset responseDataset = await response.GetValueAsync();
-
-        // assert
-        DicomSequence refSopSequence = responseDataset.GetSequence(DicomTag.ReferencedSOPSequence);
-        Assert.Single(refSopSequence);
-
-        // assert the refSopSequence has a single instances being represented
-        Assert.Single(refSopSequence.Items);
-
-        DicomDataset firstInstance = refSopSequence.Items[0];
-
-        // expect a comment sequence present
-        DicomSequence failedAttributesSequence = firstInstance.GetSequence(DicomTag.FailedAttributesSequence);
-
-        // expect comment sequence is empty as there were no errors
-        Assert.Empty(failedAttributesSequence);
     }
 
     public Task InitializeAsync() => Task.CompletedTask;
