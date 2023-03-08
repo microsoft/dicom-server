a6783ac12dbb7aa096279e2064e7a9f514844e27
Esma Dautovic <esmada@microsoft.com>
2023-03-08 15:07:12 -0800

    fix header

--- src/Microsoft.Health.Dicom.Api.UnitTests/Features/Audit/AuditMiddlewareTests.cs
+++ src/Microsoft.Health.Dicom.Api.UnitTests/Features/Audit/AuditMiddlewareTests.cs
@@ -1,8 +1,8 @@
-﻿// // -------------------------------------------------------------------------------------------------
-// // Copyright (c) Microsoft Corporation. All rights reserved.
-// // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-// // -------------------------------------------------------------------------------------------------
-//
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
 // using System.Net;
 // using System.Threading.Tasks;
 // using Microsoft.AspNetCore.Http;
