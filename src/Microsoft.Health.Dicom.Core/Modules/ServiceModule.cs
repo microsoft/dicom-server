cbe05885887194be26fc71fba325ca860b65d2f0
Abdullah Islam <31964511+abdullah248@users.noreply.github.com>
2023-03-28 16:50:34 -0500

    formatting + disalbe ohif viewer from testing

--- src/Microsoft.Health.Dicom.Core/Modules/ServiceModule.cs
+++ src/Microsoft.Health.Dicom.Core/Modules/ServiceModule.cs
@@ -91,7 +91,6 @@ public class ServiceModule : IStartupModule
             .AsSelf()
             .AsImplementedInterfaces();
 
-
         services.Add<FrameHandler>()
             .Transient()
             .AsSelf()
