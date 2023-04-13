a19b69e959fb6cf04d46914ebdb338d584dab125
Abdullah Islam <31964511+abdullah248@users.noreply.github.com>
2023-04-13 15:23:09 -0500

    Update tests

--- test/Microsoft.Health.Dicom.Web.Tests.E2E/Rest/RetrieveTransactionResourceTests.Rendered.cs
+++ test/Microsoft.Health.Dicom.Web.Tests.E2E/Rest/RetrieveTransactionResourceTests.Rendered.cs
@@ -20,6 +20,7 @@ public partial class RetrieveTransactionResourceTests
 {
     [Theory]
     [InlineData("image/jpeg")]
+    [InlineData("image/png")]
     public async Task GivenValidMediaType_WhenRetrieveRenderedInstance_ThenServerShouldReturnCorrectContentSuccessfully(string mediaType)
     {
         string studyInstanceUID = TestUidGenerator.Generate();
@@ -30,7 +31,7 @@ public partial class RetrieveTransactionResourceTests
 
         using DicomWebResponse<DicomDataset> response1 = await _instancesManager.StoreAsync(new[] { dicomFile });
 
-        using DicomWebResponse<Stream> response2 = await _client.RetrieveRenderedInstanceAsync(studyInstanceUID, seriesInstanceUID, sopInstanceUID, mediaType);
+        using DicomWebResponse<Stream> response2 = await _client.RetrieveRenderedInstanceAsync(studyInstanceUID, seriesInstanceUID, sopInstanceUID, 100, mediaType);
         Assert.True(response2.IsSuccessStatusCode);
         Assert.Equal(mediaType, response2.ContentHeaders.ContentType.MediaType);
 
@@ -74,5 +75,33 @@ public partial class RetrieveTransactionResourceTests
         DicomWebException exception = await Assert.ThrowsAsync<DicomWebException>(() => _client.RetrieveRenderedFrameAsync(studyInstanceUID, seriesInstanceUID, sopInstanceUID, 5));
 
         Assert.Equal(HttpStatusCode.NotFound, exception.StatusCode);
+    }
+
+    [Fact]
+    public async Task GivenDifferentQualities_WhenRetrieveRenderedFrame_ThenServerShouldReturnCorrectContentSuccessfully()
+    {
+        string studyInstanceUID = TestUidGenerator.Generate();
+        string seriesInstanceUID = TestUidGenerator.Generate();
+        string sopInstanceUID = TestUidGenerator.Generate();
+
+        DicomFile dicomFile = Samples.CreateRandomDicomFileWithPixelData(studyInstanceUID, seriesInstanceUID, sopInstanceUID, frames: 3);
+
+        using DicomWebResponse<DicomDataset> response1 = await _instancesManager.StoreAsync(new[] { dicomFile });
+
+        using DicomWebResponse<Stream> response2 = await _client.RetrieveRenderedFrameAsync(studyInstanceUID, seriesInstanceUID, sopInstanceUID, 0, 100);
+        Assert.True(response2.IsSuccessStatusCode);
+        Assert.Equal("image/jpeg", response2.ContentHeaders.ContentType.MediaType);
+
+        using DicomWebResponse<Stream> response3 = await _client.RetrieveRenderedFrameAsync(studyInstanceUID, seriesInstanceUID, sopInstanceUID, 1, 100);
+        Assert.True(response3.IsSuccessStatusCode);
+        Assert.Equal("image/jpeg", response2.ContentHeaders.ContentType.MediaType);
+
+        using DicomWebResponse<Stream> response4 = await _client.RetrieveRenderedFrameAsync(studyInstanceUID, seriesInstanceUID, sopInstanceUID, 0, 1);
+        Assert.True(response4.IsSuccessStatusCode);
+        Assert.Equal("image/jpeg", response4.ContentHeaders.ContentType.MediaType);
+
+        using DicomWebResponse<Stream> response5 = await _client.RetrieveRenderedFrameAsync(studyInstanceUID, seriesInstanceUID, sopInstanceUID, 1, 1);
+        Assert.True(response5.IsSuccessStatusCode);
+        Assert.Equal("image/jpeg", response5.ContentHeaders.ContentType.MediaType);
     }
 }
