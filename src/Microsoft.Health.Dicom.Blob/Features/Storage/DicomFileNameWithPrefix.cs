e24428ed93bef96b90f6be89874874c485af4606
Karthik Balasubramanian 👍 <kabalas@microsoft.com>
2023-04-14 17:17:41 -0500

    Update

--- src/Microsoft.Health.Dicom.Blob/Features/Storage/DicomFileNameWithPrefix.cs
+++ src/Microsoft.Health.Dicom.Blob/Features/Storage/DicomFileNameWithPrefix.cs
@@ -26,8 +26,12 @@ public class DicomFileNameWithPrefix : IDicomFileNameBuilder
         return $"{HashingHelper.ComputeXXHash(version, MaxPrefixLength)}_{version}_frames_range.json";
     }
 
-    // This method is used for the fallback logic to get the blob file with space in between
-    // that was introduced in a recent regression.
+    /// <summary>
+    /// This method is used for the fallback logic to get the blob file with space in between
+    /// that was introduced in a recent regression.
+    /// </summary>
+    /// <param name="version"></param>
+    /// <returns></returns>
     public string GetInstanceFramesRangeFileNameWithSpace(long version)
     {
         return $"{HashingHelper.ComputeXXHash(version, MaxPrefixLength)}_ {version}_frames_range.json";
