18eca4fc379f52111032ae41a2ecc08a7cdfabcb
Esma Dautovic <esmada@microsoft.com>
2023-05-12 12:33:27 -0700

    fix signature for e2e tests

--- src/Microsoft.Health.Dicom.Tests.Common/Extensions/IIndexDataStoreExtensions.cs
+++ src/Microsoft.Health.Dicom.Tests.Common/Extensions/IIndexDataStoreExtensions.cs
@@ -8,6 +8,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using FellowOakDicom;
 using Microsoft.Health.Dicom.Core.Features.ExtendedQueryTag;
+using Microsoft.Health.Dicom.Core.Features.Model;
 using Microsoft.Health.Dicom.Core.Features.Store;
 
 namespace Microsoft.Health.Dicom.Tests.Common.Extensions;
@@ -17,6 +18,6 @@ public static class IIndexDataStoreExtensions
     public static Task<long> BeginCreateInstanceIndexAsync(this IIndexDataStore indexDataStore, int partitionKey, DicomDataset dicomDataset, CancellationToken cancellationToken = default)
         => indexDataStore.BeginCreateInstanceIndexAsync(partitionKey, dicomDataset, Array.Empty<QueryTag>(), cancellationToken);
 
-    public static Task EndCreateInstanceIndexAsync(this IIndexDataStore indexDataStore, int partitionKey, DicomDataset dicomDataset, long watermark, bool hasFrameMetadata = false, CancellationToken cancellationToken = default)
-        => indexDataStore.EndCreateInstanceIndexAsync(partitionKey, dicomDataset, watermark, Array.Empty<QueryTag>(), true, hasFrameMetadata, cancellationToken);
+    public static Task EndCreateInstanceIndexAsync(this IIndexDataStore indexDataStore, int partitionKey, DicomDataset dicomDataset, long watermark, bool hasFrameMetadata = false, InstanceProperties instanceProperties = null, CancellationToken cancellationToken = default)
+        => indexDataStore.EndCreateInstanceIndexAsync(partitionKey, dicomDataset, watermark, Array.Empty<QueryTag>(), true, hasFrameMetadata, instanceProperties, cancellationToken);
 }
