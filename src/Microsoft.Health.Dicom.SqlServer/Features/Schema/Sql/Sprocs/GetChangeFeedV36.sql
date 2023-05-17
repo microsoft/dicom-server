a8f19efcb26685b3a8efbc67f8ffff8b55a7c121
Will Sugarman <will.sugarman@microsoft.com>
2023-05-16 21:24:36 -0700

    Add new sproc for ChangeFeed

---
+++ src/Microsoft.Health.Dicom.SqlServer/Features/Schema/Sql/Sprocs/GetChangeFeedV36.sql
@@ -0,0 +1,40 @@
+/***************************************************************************************/
+-- STORED PROCEDURE
+--     GetChangeFeedV36
+--
+-- FIRST SCHEMA VERSION
+--     36
+--
+-- DESCRIPTION
+--     Gets a stream of dicom changes (instance adds and deletes)
+--
+-- PARAMETERS
+--     @limit
+--         * Max rows to return
+--     @offet
+--         * Rows to skip
+/***************************************************************************************/
+CREATE OR ALTER PROCEDURE dbo.GetChangeFeedV36 (
+    @limit      INT,
+    @offset     BIGINT)
+AS
+BEGIN
+    SET NOCOUNT     ON
+    SET XACT_ABORT  ON
+
+    SELECT  Sequence,
+            Timestamp,
+            Action,
+            PartitionName,
+            StudyInstanceUid,
+            SeriesInstanceUid,
+            SopInstanceUid,
+            OriginalWatermark,
+            CurrentWatermark
+    FROM    dbo.ChangeFeed c
+    INNER JOIN dbo.Partition p
+    ON p.PartitionKey = c.PartitionKey
+    ORDER BY Sequence
+    OFFSET @offset ROWS
+    FETCH NEXT @limit ROWS ONLY
+END
