153fd97fdd41c73607031c8713860b74e6d4b802
Arun Mohan Kumar <amohankumar@microsoft.com>
2023-05-18 16:10:49 -0700

    Fix unit tests

--- src/Microsoft.Health.Dicom.Functions/DurableOrchestrationContextExtensions.cs
+++ src/Microsoft.Health.Dicom.Functions/DurableOrchestrationContextExtensions.cs
@@ -12,19 +12,26 @@ namespace Microsoft.Health.Dicom.Functions;
 
 internal static class DurableOrchestrationContextExtensions
 {
+    private static readonly Type ContextType = typeof(IDurableOrchestrationContext).Assembly.GetType("Microsoft.Azure.WebJobs.Extensions.DurableTask.DurableOrchestrationContext");
+
     private static readonly Func<IDurableOrchestrationContext, string> GetExecutionIdFunc = CreateGetExecutionIdFunc();
 
     public static string GetExecutionId(this IDurableOrchestrationContext context)
-        => GetExecutionIdFunc(context);
+    {
+        if (context == null || context.GetType() != ContextType)
+        {
+            return null;
+        }
+        return GetExecutionIdFunc(context);
+    }
 
     private static Func<IDurableOrchestrationContext, string> CreateGetExecutionIdFunc()
     {
-        Type contextType = typeof(IDurableOrchestrationContext).Assembly.GetType("Microsoft.Azure.WebJobs.Extensions.DurableTask.DurableOrchestrationContext");
-        PropertyInfo executionIdProperty = contextType.GetProperty("ExecutionId", BindingFlags.NonPublic | BindingFlags.Instance);
+        PropertyInfo executionIdProperty = ContextType.GetProperty("ExecutionId", BindingFlags.NonPublic | BindingFlags.Instance);
 
         // Build an expression that can be compiled into IL and loaded into the app domain
         ParameterExpression param = Expression.Parameter(typeof(IDurableOrchestrationContext), "context");
-        Expression body = Expression.Property(Expression.Convert(param, contextType), executionIdProperty);
+        Expression body = Expression.Property(Expression.Convert(param, ContextType), executionIdProperty);
         return Expression.Lambda<Func<IDurableOrchestrationContext, string>>(body, param).Compile();
     }
 }
