2ce74f22c4c335765b678f81dbb14bdfb45812dd
Esma Dautovic <esmada@microsoft.com>
2023-06-02 18:07:09 -0700

    fix tests

--- test/Microsoft.Health.Dicom.Tests.Integration/Persistence/DeleteServiceTests.cs
+++ test/Microsoft.Health.Dicom.Tests.Integration/Persistence/DeleteServiceTests.cs
@@ -72,7 +72,7 @@ public class DeleteServiceTests : IClassFixture<DeleteServiceTestsFixture>
             {
                 FileProperties fileProperties = await _fixture.FileStore.StoreFileAsync(version, stream);
 
-                Assert.Null(fileProperties);
+                Assert.NotNull(fileProperties);
             }
 
             var file = await _fixture.FileStore.GetFileAsync(version);
