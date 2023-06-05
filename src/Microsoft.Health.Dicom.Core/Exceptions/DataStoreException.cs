f4339719a284a8d0e73ffd708a1cdd7a77bd82d6
Esma Dautovic <esmada@microsoft.com>
2023-06-05 15:53:39 -0700

    attempt to get msg off of internal exception

--- src/Microsoft.Health.Dicom.Core/Exceptions/DataStoreException.cs
+++ src/Microsoft.Health.Dicom.Core/Exceptions/DataStoreException.cs
@@ -10,8 +10,7 @@ namespace Microsoft.Health.Dicom.Core.Exceptions;
 
 public class DataStoreException : ConditionalExternalException
 {
-    public DataStoreException(Exception innerException, bool isExternal = false)
-       : this(isExternal ? string.Format(CultureInfo.InvariantCulture, DicomCoreResource.ExternalDataStoreOperationFailed, innerException?.Message) : DicomCoreResource.DataStoreOperationFailed, innerException, null, isExternal)
+    public DataStoreException(Exception exception, bool isExternal = false) : this(isExternal ? string.Format(CultureInfo.InvariantCulture, DicomCoreResource.ExternalDataStoreOperationFailed, (exception?.InnerException is null ? exception.Message : exception.InnerException.Message)) : DicomCoreResource.DataStoreOperationFailed, exception, null, isExternal)
     {
     }
 
