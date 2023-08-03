51363e9c6387e7e62e8a73bff22d36490a876c4d
Will Sugarman <will.sugarman@microsoft.com>
2023-08-03 12:24:35 -0700

    Normalize Cancellation Handling for STOW (#2877)
    Return 400 for canceled STOW requests

--- src/Microsoft.Health.Dicom.Core/Features/Store/StoreService.cs
+++ src/Microsoft.Health.Dicom.Core/Features/Store/StoreService.cs
@@ -191,24 +191,18 @@ public class StoreService : IStoreService
                 }
             }
         }
+        catch (OperationCanceledException)
+        {
+            throw;
+        }
         catch (Exception ex)
         {
-            ushort failureCode = FailureReasonCodes.ProcessingFailure;
-
-            switch (ex)
+            ushort failureCode = ex switch
             {
-                case DicomValidationException _:
-                    failureCode = FailureReasonCodes.ValidationFailure;
-                    break;
-
-                case DatasetValidationException dicomDatasetValidationException:
-                    failureCode = dicomDatasetValidationException.FailureCode;
-                    break;
-
-                case ValidationException _:
-                    failureCode = FailureReasonCodes.ValidationFailure;
-                    break;
-            }
+                DatasetValidationException dve => dve.FailureCode,
+                DicomValidationException or ValidationException => FailureReasonCodes.ValidationFailure,
+                _ => FailureReasonCodes.ProcessingFailure,
+            };
 
             LogValidationFailedDelegate(_logger, index, failureCode, ex);
 
@@ -234,26 +228,22 @@ public class StoreService : IStoreService
             );
             return length;
         }
+        catch (ConditionalExternalException cee) when (cee.IsExternal)
+        {
+            throw;
+        }
+        catch (DataStoreException dse) when (dse.InnerException is OperationCanceledException)
+        {
+            throw;
+        }
         catch (Exception ex)
         {
-            ushort failureCode = FailureReasonCodes.ProcessingFailure;
-
-            switch (ex)
+            ushort failureCode = ex switch
             {
-                case DataStoreException { IsExternal: true }:
-                    throw;
-
-                case DataStoreRequestFailedException { IsExternal: true }:
-                    throw;
-
-                case PendingInstanceException _:
-                    failureCode = FailureReasonCodes.PendingSopInstance;
-                    break;
-
-                case InstanceAlreadyExistsException _:
-                    failureCode = FailureReasonCodes.SopInstanceAlreadyExists;
-                    break;
-            }
+                PendingInstanceException => FailureReasonCodes.PendingSopInstance,
+                InstanceAlreadyExistsException => FailureReasonCodes.SopInstanceAlreadyExists,
+                _ => FailureReasonCodes.ProcessingFailure,
+            };
 
             LogFailedToStoreDelegate(_logger, index, failureCode, ex);
 
