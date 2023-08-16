38d905dfc79f6147b4d7834ef819cff28cedd738
Abdullah Islam <31964511+abdullah248@users.noreply.github.com>
2023-08-16 15:28:25 -0500

    encode response

--- src/Microsoft.Health.Dicom.Core/Exceptions/InvalidQueryStringValuesException.cs
+++ src/Microsoft.Health.Dicom.Core/Exceptions/InvalidQueryStringValuesException.cs
@@ -1,16 +1,17 @@
-﻿// -------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
 using System.Globalization;
+using System.Web;
 
 namespace Microsoft.Health.Dicom.Core.Exceptions;
 
 public class InvalidQueryStringValuesException : ValidationException
 {
     public InvalidQueryStringValuesException(string key, string error)
-        : base(string.Format(CultureInfo.InvariantCulture, DicomCoreResource.InvalidQueryStringValue, key, error))
+        : base(string.Format(CultureInfo.InvariantCulture, DicomCoreResource.InvalidQueryStringValue, key, HttpUtility.UrlEncode(error)))
     {
     }
 }
