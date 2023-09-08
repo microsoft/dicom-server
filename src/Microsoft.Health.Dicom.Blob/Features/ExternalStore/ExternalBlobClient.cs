dac11f8c838d2e9fb812ea576cd3dbbe280a4aaa
Esma Dautovic <esmada@microsoft.com>
2023-09-08 16:50:59 -0700

    ensure file props passed in for condition not null

--- src/Microsoft.Health.Dicom.Blob/Features/ExternalStore/ExternalBlobClient.cs
+++ src/Microsoft.Health.Dicom.Blob/Features/ExternalStore/ExternalBlobClient.cs
@@ -104,6 +104,7 @@ internal class ExternalBlobClient : IBlobClient
 
     public BlobRequestConditions GetConditions(FileProperties fileProperties)
     {
+        EnsureArg.IsNotNull(fileProperties);
         return new BlobRequestConditions // ensure file has not been changed since we last worked with it
         {
             IfMatch = new ETag(fileProperties.ETag),
