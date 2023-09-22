14f31e3f0a4e71d51f41e1284061b40d787c1769
Abdullah Islam <31964511+abdullah248@users.noreply.github.com>
2023-09-22 14:16:57 -0500

    pr comment

--- test/Microsoft.Health.Dicom.Tests.Integration/Persistence/IndexDataStoreTests.cs
+++ test/Microsoft.Health.Dicom.Tests.Integration/Persistence/IndexDataStoreTests.cs
@@ -629,7 +629,7 @@ public partial class IndexDataStoreTests : IClassFixture<SqlDataStoreTestsFixtur
     }
 
     [Fact]
-    public async Task GivenMultipleNewInstancesInSameStudy_WhenAddedInParallel_ThenItShouldBeAdded()
+    public Task GivenMultipleNewInstancesInSameStudy_WhenAddedInParallel_ThenItShouldBeAdded()
     {
         string studyInstanceUid = TestUidGenerator.Generate();
         string seriesInstanceUid = TestUidGenerator.Generate();
@@ -637,14 +637,14 @@ public partial class IndexDataStoreTests : IClassFixture<SqlDataStoreTestsFixtur
         DicomDataset instance1 = CreateTestDicomDataset(studyInstanceUid, seriesInstanceUid);
         DicomDataset instance2 = CreateTestDicomDataset(studyInstanceUid, seriesInstanceUid);
 
-        await Task.WhenAll(
+        return Task.WhenAll(
             _indexDataStore.BeginCreateInstanceIndexAsync(new Partition(1, "clinic-one"), instance1),
             _indexDataStore.BeginCreateInstanceIndexAsync(new Partition(1, "clinic-one"), instance2));
     }
 
 
     [Fact]
-    public async Task GivenMultipleNewInstancesInDifferentStudy_SameSeriesAndSopInstanceUID_ThenItShouldBeAdded()
+    public Task GivenMultipleNewInstancesInDifferentStudy_SameSeriesAndSopInstanceUID_ThenItShouldBeAdded()
     {
         string studyInstanceUid1 = TestUidGenerator.Generate();
         string studyInstanceUid2 = TestUidGenerator.Generate();
@@ -654,7 +654,7 @@ public partial class IndexDataStoreTests : IClassFixture<SqlDataStoreTestsFixtur
         DicomDataset instance1 = CreateTestDicomDataset(studyInstanceUid1, seriesInstanceUid, sopInstanceUid);
         DicomDataset instance2 = CreateTestDicomDataset(studyInstanceUid2, seriesInstanceUid, sopInstanceUid);
 
-        await Task.WhenAll(
+        return Task.WhenAll(
             _indexDataStore.BeginCreateInstanceIndexAsync(new Partition(1, "clinic-one"), instance1),
             _indexDataStore.BeginCreateInstanceIndexAsync(new Partition(1, "clinic-one"), instance2));
     }
