9899846ef53ec21892d32f1f1e554266560a2abb
Esma <69813504+esmadau@users.noreply.github.com>
2023-10-05 13:38:24 -0700

    File Store metrics (#3071)
    Start emitting a log and metric on each file store operation. Add stream length dimension when one is present

---
+++ src/Microsoft.Health.Dicom.Blob/Features/Telemetry/BlobFileStoreMeter.cs
@@ -0,0 +1,57 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using Microsoft.Health.Dicom.Core.Features.Telemetry;
+
+namespace Microsoft.Health.Dicom.Blob.Features.Telemetry;
+
+public sealed class BlobFileStoreMeter : IDisposable
+{
+    private readonly Meter _meter;
+
+    public BlobFileStoreMeter()
+    {
+        _meter = new Meter($"{OpenTelemetryLabels.BaseMeterName}.BlobFileStore", "1.0");
+
+        BlobFileStoreOperationCount =
+            _meter.CreateCounter<int>(nameof(BlobFileStoreOperationCount),
+                description: "A blob file store operation was hit.");
+
+        BlobFileStoreOperationStreamSize =
+            _meter.CreateCounter<long>(nameof(BlobFileStoreOperationStreamSize),
+                description: "The stream size being processed fo I/O.");
+    }
+
+    /// <summary>
+    /// Represents a call to the blob file store operation
+    /// </summary>
+    public Counter<int> BlobFileStoreOperationCount { get; }
+
+    /// <summary>
+    /// streaming size given an operation
+    /// </summary>
+    public Counter<long> BlobFileStoreOperationStreamSize { get; }
+
+    /// <summary>
+    /// Sets telemetry dimensions on meter
+    /// </summary>
+    /// <param name="operationName">Name of operation being hit</param>
+    /// <param name="operationType">Represents whether operation is input (write) or output(read) </param>
+    /// <param name="isExternal">Whether or not this metric is being emitted for an external store</param>
+    /// <returns></returns>
+    public static KeyValuePair<string, object>[] CreateBlobFileStoreOperationTelemetryDimension(string operationName, OperationType operationType, bool isExternal) =>
+        new[]
+        {
+            new KeyValuePair<string, object>("Operation", operationName),
+            new KeyValuePair<string, object>("Type", operationType),
+            new KeyValuePair<string, object>("IsExternal", isExternal),
+        };
+
+    public void Dispose()
+        => _meter.Dispose();
+}
---
+++ src/Microsoft.Health.Dicom.Blob/Features/Telemetry/OperationType.cs
@@ -0,0 +1,22 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+namespace Microsoft.Health.Dicom.Blob.Features.Telemetry;
+
+/// <summary>
+/// Represents whether operation is input (write) or output(read) from perspective of I/O on the blob store
+/// </summary>
+public enum OperationType
+{
+    /// <summary>
+    /// For Operations that write data
+    /// </summary>
+    Input,
+
+    /// <summary>
+    /// For Operations that read data
+    /// </summary>
+    Output
+}
