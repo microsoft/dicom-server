1769e69fc5634b22f283d3ec43d7192b89dc5300
Julie Lycklama <julyckla@microsoft.com>
2023-10-10 14:18:47 -0700

    add cmk check to durable task health check

--- src/Microsoft.Health.Dicom.Functions.Client.UnitTests/HealthChecks/DurableTaskHealthCheckTests.cs
+++ src/Microsoft.Health.Dicom.Functions.Client.UnitTests/HealthChecks/DurableTaskHealthCheckTests.cs
@@ -7,8 +7,10 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Health.Core.Features.Health;
 using Microsoft.Health.Dicom.Functions.Client.HealthChecks;
 using Microsoft.Health.Dicom.Functions.Client.TaskHub;
+using Microsoft.Health.Encryption.Customer.Health;
 using NSubstitute;
 using Xunit;
 
@@ -17,11 +19,17 @@ namespace Microsoft.Health.Dicom.Functions.Client.UnitTests.HealthChecks;
 public class DurableTaskHealthCheckTests
 {
     private readonly ITaskHubClient _client = Substitute.For<ITaskHubClient>();
+    private readonly ValueCache<CustomerKeyHealth> _customerKeyHealthCache = new ValueCache<CustomerKeyHealth>();
     private readonly DurableTaskHealthCheck _healthCheck;
 
     public DurableTaskHealthCheckTests()
     {
-        _healthCheck = new DurableTaskHealthCheck(_client, NullLogger<DurableTaskHealthCheck>.Instance);
+        _customerKeyHealthCache.Set(new CustomerKeyHealth
+        {
+            IsHealthy = true,
+        });
+
+        _healthCheck = new DurableTaskHealthCheck(_client, _customerKeyHealthCache, NullLogger<DurableTaskHealthCheck>.Instance);
     }
 
     [Fact]
@@ -67,5 +75,19 @@ public class DurableTaskHealthCheckTests
 
         await _client.Received(1).GetTaskHubAsync(tokenSource.Token);
         await taskHub.Received(1).IsHealthyAsync(tokenSource.Token);
+    }
+
+    [Fact]
+    public async Task GivenPrerequisiteIsNotHealthy_WhenCheckingHealth_ThenReturnDegraded()
+    {
+        using var tokenSource = new CancellationTokenSource();
+        _customerKeyHealthCache.Set(new CustomerKeyHealth
+        {
+            IsHealthy = false,
+            Reason = HealthStatusReason.CustomerManagedKeyAccessLost,
+        });
+
+        HealthCheckResult actual = await _healthCheck.CheckHealthAsync(new HealthCheckContext(), tokenSource.Token);
+        Assert.Equal(HealthStatus.Degraded, actual.Status);
     }
 }
