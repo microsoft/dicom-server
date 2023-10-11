1d9a2641baeaf2f424327515619494a3651c5cf2
Will Sugarman <will.sugarman@microsoft.com>
2023-10-11 10:45:23 -0700

    Disable Test Parallelization for SQL Auth Tests (#3099)
    Disable parallelization for sql auth

---
+++ src/Microsoft.Health.Dicom.SqlServer.UnitTests/SqlAuthenticationCollection.cs
@@ -0,0 +1,13 @@
+// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using Xunit;
+
+namespace Microsoft.Health.Dicom.SqlServer.UnitTests;
+
+[CollectionDefinition("SQL Authentication Collection", DisableParallelization = true)]
+public class SqlAuthenticationCollection
+{
+}
