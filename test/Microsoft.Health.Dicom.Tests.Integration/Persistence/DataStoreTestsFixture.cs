31407d72c61c483dfb268f4b5bed5165c8843801
Karthik Balasubramanian 👍 <kabalas@microsoft.com>
2023-10-25 09:52:59 -0700

    Revert "Added test to verify cold tier"
    This reverts commit 02156c5e532d01a16e190ede1f2899e8ff80cd0e.

--- test/Microsoft.Health.Dicom.Tests.Integration/Persistence/DataStoreTestsFixture.cs
+++ test/Microsoft.Health.Dicom.Tests.Integration/Persistence/DataStoreTestsFixture.cs
@@ -101,8 +101,6 @@ public class DataStoreTestsFixture : IAsyncLifetime
         }
     }
 
-    public BlobContainerClient GetBlobContainerClient() => _blobClient.GetBlobContainerClient(_blobContainerConfiguration.ContainerName);
-
     private class TestInternalBlobClient : IBlobClient
     {
         public TestInternalBlobClient(BlobServiceClient blobClient, string containerName)
