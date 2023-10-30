4d7abe1aba7494ba50f8d9aaed164f9eca3fcf86
Will Sugarman <will.sugarman@microsoft.com>
2023-10-30 11:57:49 -0700

    Remove using statement

--- test/Microsoft.Health.Dicom.Web.Tests.E2E/Common/DicomTagsManager.cs
+++ test/Microsoft.Health.Dicom.Web.Tests.E2E/Common/DicomTagsManager.cs
@@ -6,7 +6,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
