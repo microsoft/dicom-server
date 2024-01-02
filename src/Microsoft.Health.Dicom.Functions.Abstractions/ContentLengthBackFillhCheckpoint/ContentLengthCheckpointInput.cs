4c4a4134835f2777726db2fda83c2db50b0eaee1
Esma Dautovic <esmada@microsoft.com>
2024-01-02 12:26:12 -0800

    fix nits

--- src/Microsoft.Health.Dicom.Functions.Abstractions/ContentLengthBackFillhCheckpoint/ContentLengthCheckpointInput.cs
+++ src/Microsoft.Health.Dicom.Functions.Abstractions/ContentLengthBackFillhCheckpoint/ContentLengthCheckpointInput.cs
@@ -3,7 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-
 namespace Microsoft.Health.Dicom.Functions.ContentLengthBackFill;
 
 public class ContentLengthBackFillInput
