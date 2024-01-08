c7500f80f6be1f3ab41804c2990862ec2cb7273a
Esma Dautovic <esmada@microsoft.com>
2024-01-08 10:54:08 -0800

    consolidate dupe tests

--- src/Microsoft.Health.Dicom.Core.UnitTests/Features/Validation/UidValidationTests.cs
+++ src/Microsoft.Health.Dicom.Core.UnitTests/Features/Validation/UidValidationTests.cs
@@ -1,8 +1,10 @@
-// -------------------------------------------------------------------------------------------------
+﻿// -------------------------------------------------------------------------------------------------
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using FellowOakDicom;
+using Microsoft.Health.Dicom.Core.Exceptions;
 using Microsoft.Health.Dicom.Core.Features.Validation;
 using Xunit;
 
@@ -10,33 +12,53 @@ namespace Microsoft.Health.Dicom.Core.UnitTests.Features.Validation;
 
 public class UidValidationTests
 {
-    [Theory]
-    [InlineData(null)]
-    [InlineData("")]
-    public void GivenNullOrEmpty_WhenValidatingUidThatAllowsEmpty_ThenShouldPass(string value)
-        => Assert.True(UidValidation.IsValid(value, allowEmpty: true));
 
     [Theory]
+    [InlineData("13.14.520")]
+    [InlineData("13.14.0520")]
+    [InlineData("13.14.520\0")]
+    [InlineData("13")]
     [InlineData("0")]
     [InlineData("0.2.4.6.8")]
     [InlineData("98.0.705.456.1.52365")]
     [InlineData("123.0.45.6345.16765.0")]
     [InlineData("12.0.0.678.324.145.123106.141.4905702.123480.9500026724.0.1.4020")]
     [InlineData("12.0.0.678.324.145.123106.141.4905702.123480.9500026724.0.1.4020    ")]
-    public void GivenValidString_WhenValidatingAsUid_ThenShouldPass(string value)
-        => Assert.True(UidValidation.IsValid(value));
+    public void GivenValidateUid_WhenValidating_ThenShouldPass(string value)
+    {
+        DicomElement element = new DicomUniqueIdentifier(DicomTag.DigitalSignatureUID, value);
+        new UidValidation().Validate(element);
+    }
 
     [Theory]
-    [InlineData(null)]
     [InlineData("")]
-    [InlineData("007")]
-    [InlineData("12.003.456")]
-    [InlineData("hello.world")]
-    [InlineData("987.in.valid.654")]
-    [InlineData("24.foo.bar.baz")]
-    [InlineData("98-0-705-456-1-52365")]
-    [InlineData("123-0-45-6345-16765-0")]
-    [InlineData("12.0.0.678.324.145.123106.141.4905702.123480.9500026724.0.1.40201")]
-    public void GivenInvalidString_WhenValidatingAsUid_ThenShouldFail(string value)
-        => Assert.False(UidValidation.IsValid(value));
+    [InlineData("\0")]
+    [InlineData(null)]
+    public void GivenValidateUid_WhenValidatingNullOrEmpty_ThenShouldNotPass(string value)
+    {
+        DicomElement element = new DicomUniqueIdentifier(DicomTag.DigitalSignatureUID, value);
+        new UidValidation().Validate(element);
+    }
+
+    [Fact]
+    public void GivenMultipleValues_WhenValidating_ThenShouldVaidateFirstOne()
+    {
+        DicomElement element = new DicomUniqueIdentifier(DicomTag.DigitalSignatureUID, "13", "11|");
+        new UidValidation().Validate(element);
+    }
+
+    [Theory]
+    [InlineData("123.")] // end with .
+    [InlineData("abc.123")] // a is invalid character
+    [InlineData("11|")] // | is invalid character
+    [InlineData("0123456789012345678901234567890123456789012345678901234567890123456789")] // value is too long
+    [InlineData("12.003.456")] // not enough segments
+    [InlineData("987.111.111a.654")] // segment itself invalid as it contains alpha chars
+    [InlineData("98-0-705-456-1-52365")] // segments should be separated by .
+    public void GivenInvalidUidWhenValidating_ThenShouldThrow(string value)
+    {
+        DicomElement element = new DicomUniqueIdentifier(DicomTag.DigitalSignatureUID, value);
+        Assert.Throws<InvalidIdentifierException>(() => new UidValidation().Validate(element));
+    }
+
 }
