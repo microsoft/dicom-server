1b5b2dad8e2454bb5cea936d0c7d3c30386eb8e8
Esma Dautovic <esmada@microsoft.com>
2024-02-02 15:02:45 -0800

    remove unnecessary counters

--- src/Microsoft.Health.Dicom.Functions.UnitTests/IndexMetricsCollection/IndexMetricsCollectionFunctionTests.cs
+++ src/Microsoft.Health.Dicom.Functions.UnitTests/IndexMetricsCollection/IndexMetricsCollectionFunctionTests.cs
@@ -3,8 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -12,13 +10,8 @@ using Microsoft.Extensions.Options;
 using Microsoft.Health.Dicom.Core.Configs;
 using Microsoft.Health.Dicom.Core.Features.Common;
 using Microsoft.Health.Dicom.Core.Features.Store;
-using Microsoft.Health.Dicom.Core.Features.Telemetry;
 using Microsoft.Health.Dicom.Functions.MetricsCollection;
-using Microsoft.Health.Dicom.Functions.MetricsCollection.Telemetry;
 using NSubstitute;
-using NSubstitute.ExceptionExtensions;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
 using Xunit;
 
 namespace Microsoft.Health.Dicom.Functions.UnitTests.IndexMetricsCollection;
@@ -27,37 +20,15 @@ public class IndexMetricsCollectionFunctionTests
 {
     private readonly IndexMetricsCollectionFunction _collectionFunction;
     private readonly IIndexDataStore _indexStore;
-    private readonly IndexMetricsCollectionMeter _meter;
-    private readonly List<Metric> _exportedItems;
-    private readonly MeterProvider _meterProvider;
     private readonly TimerInfo _timer;
 
     public IndexMetricsCollectionFunctionTests()
     {
-        string meterName = Guid.NewGuid().ToString();
-        _meter = new IndexMetricsCollectionMeter(meterName);
-        _exportedItems = new List<Metric>();
-        _meterProvider = Sdk.CreateMeterProviderBuilder()
-            .AddMeter(meterName)
-            .AddInMemoryExporter(_exportedItems)
-            .Build();
         _indexStore = Substitute.For<IIndexDataStore>();
         _collectionFunction = new IndexMetricsCollectionFunction(
             _indexStore,
-            Options.Create(new FeatureConfiguration { EnableExternalStore = true, }),
-            _meter);
+            Options.Create(new FeatureConfiguration { EnableExternalStore = true, }));
         _timer = Substitute.For<TimerInfo>(default, default, default);
-    }
-
-    [Fact]
-    public async Task GivenIndexMetricsCollectionFunction_WhenRunException_ThenIndexMetricsCollectionsCompletedCounterIsIncremented()
-    {
-        _indexStore.GetIndexedFileMetricsAsync().ThrowsForAnyArgs(new Exception());
-
-        await Assert.ThrowsAsync<Exception>(() => _collectionFunction.Run(_timer, NullLogger.Instance));
-
-        _meterProvider.ForceFlush();
-        AssertMetricEmitted(_meter.IndexMetricsCollectionsCompletedCounter.Name, _exportedItems, expectedSucceededTagValue: false);
     }
 
     [Fact]
@@ -68,9 +39,6 @@ public class IndexMetricsCollectionFunctionTests
         await _collectionFunction.Run(_timer, NullLogger.Instance);
 
         await _indexStore.ReceivedWithAnyArgs(1).GetIndexedFileMetricsAsync();
-
-        _meterProvider.ForceFlush();
-        AssertMetricEmitted(_meter.IndexMetricsCollectionsCompletedCounter.Name, _exportedItems);
     }
 
     [Fact]
@@ -79,26 +47,10 @@ public class IndexMetricsCollectionFunctionTests
         _indexStore.GetIndexedFileMetricsAsync().ReturnsForAnyArgs(new IndexedFileProperties());
         var collectionFunctionWihtoutExternalStore = new IndexMetricsCollectionFunction(
             _indexStore,
-            Options.Create(new FeatureConfiguration { EnableExternalStore = false, }),
-            _meter);
+            Options.Create(new FeatureConfiguration { EnableExternalStore = false, }));
 
         await collectionFunctionWihtoutExternalStore.Run(_timer, NullLogger.Instance);
 
         await _indexStore.DidNotReceiveWithAnyArgs().GetIndexedFileMetricsAsync();
-        _meterProvider.ForceFlush();
-        Assert.Empty(_exportedItems);
-    }
-
-    private static void AssertMetricEmitted(
-        string metricName,
-        List<Metric> exportedItems,
-        bool expectedSucceededTagValue = true)
-    {
-        Assert.NotEmpty(exportedItems);
-        IReadOnlyList<MetricPoint> points = exportedItems.GetMetricPoints(metricName);
-        Assert.Single(points);
-
-        Dictionary<string, object> tags = points[0].GetTags();
-        Assert.Equal(expectedSucceededTagValue, tags["CollectionSucceeded"]);
     }
 }
--- src/Microsoft.Health.Dicom.Functions/MetricsCollection/IndexMetricsCollectionFunction.cs
+++ src/Microsoft.Health.Dicom.Functions/MetricsCollection/IndexMetricsCollectionFunction.cs
@@ -3,8 +3,6 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-using System;
-using System.Diagnostics;
 using System.Threading.Tasks;
 using EnsureThat;
 using Microsoft.Azure.WebJobs;
@@ -13,7 +11,6 @@ using Microsoft.Extensions.Options;
 using Microsoft.Health.Dicom.Core.Configs;
 using Microsoft.Health.Dicom.Core.Features.Common;
 using Microsoft.Health.Dicom.Core.Features.Store;
-using Microsoft.Health.Dicom.Functions.MetricsCollection.Telemetry;
 using Microsoft.Health.Functions.Extensions;
 
 namespace Microsoft.Health.Dicom.Functions.MetricsCollection;
@@ -24,19 +21,16 @@ namespace Microsoft.Health.Dicom.Functions.MetricsCollection;
 public class IndexMetricsCollectionFunction
 {
     private readonly IIndexDataStore _indexDataStore;
-    private readonly IndexMetricsCollectionMeter _meter;
     private readonly bool _externalStoreEnabled;
     private readonly bool _enableDataPartitions;
     private const string RunFrequencyVariable = $"%{AzureFunctionsJobHost.RootSectionName}:DicomFunctions:{IndexMetricsCollectionOptions.SectionName}:{nameof(IndexMetricsCollectionOptions.Frequency)}%";
 
     public IndexMetricsCollectionFunction(
         IIndexDataStore indexDataStore,
-        IOptions<FeatureConfiguration> featureConfiguration,
-        IndexMetricsCollectionMeter meter)
+        IOptions<FeatureConfiguration> featureConfiguration)
     {
         EnsureArg.IsNotNull(featureConfiguration, nameof(featureConfiguration));
         _indexDataStore = EnsureArg.IsNotNull(indexDataStore, nameof(indexDataStore));
-        _meter = EnsureArg.IsNotNull(meter, nameof(meter));
         _externalStoreEnabled = featureConfiguration.Value.EnableExternalStore;
         _enableDataPartitions = featureConfiguration.Value.EnableDataPartitions;
     }
@@ -60,39 +54,17 @@ public class IndexMetricsCollectionFunction
             return;
         }
 
-        log.LogInformation("Collecting a daily summation starting. At: {Timestamp}", DateTime.UtcNow);
         if (invocationTimer.IsPastDue)
         {
             log.LogWarning("Current function invocation is running late.");
         }
-        try
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            IndexedFileProperties indexedFileProperties = await _indexDataStore.GetIndexedFileMetricsAsync();
-            stopwatch.Stop();
+        IndexedFileProperties indexedFileProperties = await _indexDataStore.GetIndexedFileMetricsAsync();
 
-            log.LogInformation(
-                "DICOM telemetry - TotalFilesIndexed: {TotalFilesIndexed} , TotalByesIndexed: {TotalContentLengthIndexed} ,  with ExternalStoreEnabled: {ExternalStoreEnabled} and DataPartitionsEnabled: {PartitionsEnabled}",
-                indexedFileProperties.TotalIndexed,
-                indexedFileProperties.TotalSum,
-                _externalStoreEnabled,
-                _enableDataPartitions);
-
-            _meter.IndexMetricsCollectionsCompletedCounter.Add(1,
-                IndexMetricsCollectionMeter.CreateTelemetryDimension(succeeded: true, _externalStoreEnabled, _enableDataPartitions));
-
-            log.LogInformation(
-                "Collecting a daily summation time taken: {ElapsedTime} ms with ExternalStoreEnabled: {ExternalStoreEnabled} and DataPartitionsEnabled: {PartitionsEnabled}",
-                stopwatch.ElapsedMilliseconds,
-                _externalStoreEnabled,
-                _enableDataPartitions);
-        }
-        catch (Exception)
-        {
-            _meter.IndexMetricsCollectionsCompletedCounter.Add(1,
-                IndexMetricsCollectionMeter.CreateTelemetryDimension(succeeded: false, _externalStoreEnabled, _enableDataPartitions));
-            throw;
-        }
+        log.LogInformation(
+            "DICOM telemetry - TotalFilesIndexed: {TotalFilesIndexed} , TotalByesIndexed: {TotalContentLengthIndexed} ,  with ExternalStoreEnabled: {ExternalStoreEnabled} and DataPartitionsEnabled: {PartitionsEnabled}",
+            indexedFileProperties.TotalIndexed,
+            indexedFileProperties.TotalSum,
+            _externalStoreEnabled,
+            _enableDataPartitions);
     }
 }
