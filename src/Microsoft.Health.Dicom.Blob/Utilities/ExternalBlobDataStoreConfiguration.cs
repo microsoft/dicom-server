b4e2210227daebcc4f3493c5ccfb9d493135257e
Julie Lycklama <julyckla@microsoft.com>
2024-02-06 11:21:34 -0800

    add default for health check expiry

--- src/Microsoft.Health.Dicom.Blob/Utilities/ExternalBlobDataStoreConfiguration.cs
+++ src/Microsoft.Health.Dicom.Blob/Utilities/ExternalBlobDataStoreConfiguration.cs
@@ -23,7 +23,7 @@ internal class ExternalBlobDataStoreConfiguration
     public string HealthCheckFilePath { get; set; }
 
     [Range(typeof(TimeSpan), "00:01:00", "1.00:00:00", ConvertValueInInvariantCulture = true, ParseLimitsInInvariantCulture = true)]
-    public TimeSpan HealthCheckFileExpiry { get; set; }
+    public TimeSpan HealthCheckFileExpiry { get; set; } = TimeSpan.FromMinutes(5);
 
     /// <summary>
     /// A path which is used to store blobs along a specific path in a container, serving as a prefix to the
