d20e0cb85963fb93452e6ab8a9cf85777ae623c5
Esma Dautovic <esmada@microsoft.com>
2024-02-07 16:28:27 -0800

    fix namespace

--- src/Microsoft.Health.Dicom.Api/Features/Telemetry/DicomTelemetry.cs
+++ src/Microsoft.Health.Dicom.Api/Features/Telemetry/DicomTelemetry.cs
@@ -3,8 +3,7 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
-namespace Microsoft.Health.Dicom.Core.Features.Telemetry;
-
+namespace Microsoft.Health.Dicom.Api.Features.Telemetry;
 internal static class DicomTelemetry
 {
     public const string ContextItemPrefix = "Dicom_";
